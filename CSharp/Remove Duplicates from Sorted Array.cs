3658c96d521927e675df2b0a1de09fb327a23765
XinRuoShi <XinruoShi>
2019-05-27 12:17:52 +0800

    26 27

---
+++ CSharp/Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,67 @@
+﻿//Source  : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+//Author  : Xinruo Shi
+//Date    : 2019-05-20
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * 
+ * Input: nums = [1,1,2]
+ * Output: 2 (nums=[1,2...])
+ * 
+ * Input: nums = [0,0,1,1,1,2,2,3,3,4]
+ * Output: 5 ([nums=[0,1,2,3,4....])
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution
+{
+    // --------------- O(n) 292ms --------------- 31.5MB ---------------
+    /*
+     * When null or length=0, we should return 0 not 1
+     */
+    public int RemoveDuplicates_1(int[] nums)
+    {       
+        if (nums.Length == 0 || nums == null)
+        {
+            return 0;
+        }
+
+        int n = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+        return n;
+    }
+
+    // --------------- O(n) 260ms --------------- 31.7MB ---------------
+    /*
+     * asymptotic time complexity for this solution is least 
+     * Because this compare nums[n] ,it cost time less than nums[i-1]
+     */
+    public int RemoveDuplicates_2(int[] nums)
+    {
+        if (nums.Length < 2)
+            return nums.Length;
+
+        int n = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[n] != nums[i])
+            {
+                nums[++n] = nums[i];
+            }
+        }
+        return n + 1;
+    } 
+}
+/**************************************************************************************************************
+ * RemoveDuplicates_1 RemoveDuplicates_2                                                                      *
+ **************************************************************************************************************/
---
+++ CSharp/Remove Element.cs
@@ -0,0 +1,75 @@
+﻿//Source  : https://leetcode.com/problems/remove-element/
+//Author  : Xinruo Shi
+//Date    : 2019-05-27
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * 
+ * Input: nums = [3,2,2,3], val = 3
+ * Output: length = 2 (nums=[2,2,....])
+ * 
+ * Input: nums = [0,1,2,2,3,0,4,2], val = 2
+ * Output: length = 5 ([nums=[0,1,3,0,4,....]) Note that the order of those five elements can be arbitrary.
+ * 
+ *******************************************************************************************************************************/
+public class Solution
+{
+    // --------------- O(n) 248ms --------------- 28.4MB ---------------
+    public int RemoveElement_1(int[] nums, int val)
+    {
+        int temp = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == val)
+            {
+                temp++;             
+            }
+        }
+
+        int n = nums.Length - temp;
+        int m = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i]==val)
+            {
+                int t = nums[i];
+                nums[i] = nums[nums.Length-1 - m];
+                nums[nums.Length-1 - m] = t;
+                m++;
+                i--;
+            }
+        }
+        return n;
+    }
+
+    // --------------- O(n) 236ms --------------- 28.1MB ---------------
+    /*
+     * !!! It doesn't matter what values are set beyond the returned length. !!!
+     * 
+     */
+    public int RemoveElement_2(int[] nums, int val)
+    {
+        if (nums.Length == 0 || nums == null)
+        {
+            return 0;
+        }
+
+        int n = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[n] = nums[i];
+                n++;
+            }
+        }        
+        return n;
+    }
+}
+/**************************************************************************************************************
+ * RemoveElement_2                                                                                            *
+ **************************************************************************************************************/
