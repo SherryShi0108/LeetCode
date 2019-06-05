476951f9ece477aeba3e3c29fd373fcff07921cf
XinRuoShi <XinruoShi>
2019-06-05 14:34:06 +0800

    121 169

---
+++ CSharp/Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,92 @@
+﻿//Source  : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+//Author  : Xinruo Shi
+//Date    : 2019-06-02
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Say you have an array for which the i^th element is the price of a given stock on day i.
+ * 
+ * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), 
+ * design an algorithm to find the maximum profit.
+ * Note that you cannot sell a stock before you buy one.
+ * 
+ * Input:  [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ *              Not 7-1 = 6, as selling price needs to be larger than buying price.
+ *
+ * Input:  [7,6,4,3,1]
+ * Output: 0
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution121
+{
+    // ---------------Brute Force --------------- O(n^2) 892ms --------------- 23.4MB ---------------
+    public int MaxProfit_1(int[] nums)
+    {
+        int max = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if ((nums[j] - nums[i]) > max && (nums[j] - nums[i]) > 0)
+                {
+                    max = nums[j] - nums[i];
+                }
+            }
+        }
+
+        return max;
+    }
+
+    // --------------- Kadane's Algorithm --------------- O(n) 100ms --------------- 23.3MB --------------- 
+    public int MaxProfit_2(int[] nums)
+    {
+        int sum = 0;
+        int max = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (sum + (nums[i] - nums[i - 1]) > 0)
+            {
+                sum = sum + (nums[i] - nums[i - 1]);
+            }
+            else
+            {
+                sum = 0;
+            }
+            max = max >= sum ? max : sum;
+        }
+
+        return max;
+    }
+
+    // --------------- O(n) 104ms --------------- 23.4MB ---------------
+    public int MaxProfit_3(int[] nums)
+    {
+        if (nums.Length == 0 || nums == null)
+        {
+            return 0;
+        }
+
+        int max = 0;
+        int min = nums[0];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            max = max > nums[i] - min ? max : nums[i] - min;
+            min = min > nums[i] ? nums[i] : min;
+        }
+        return max;
+    }
+
+    // --------------- dynamic programming  --------------- 
+    public int MaxProfit_4(int[] nums)
+    {
+
+        return 0;
+    }
+}
+/**************************************************************************************************************
+ * MaxProfit_2 MaxProfit_3                                                                                    *
+ **************************************************************************************************************/
---
+++ CSharp/Majority Element.cs
@@ -0,0 +1,82 @@
+﻿//Source  : https://leetcode.com/problems/majority-element/
+//Author  : Xinruo Shi
+//Date    : 2019-06-05
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ * 
+ * Input: nums=[3,2,3]
+ * Output: 3
+ * 
+ * Input: nums=[2,2,1,1,1,2,2]
+ * Output: 2
+ * 
+ *******************************************************************************************************************************/
+
+using System.Collections.Generic; // Dictionary资源字典
+
+public class Solution169
+{
+    // --------------- HashMap --------------- O(n)120ms --------------- O(n)27.6MB ---------------
+    public int MajorityElement_1(int[] nums)
+    {
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (d.ContainsKey(nums[i]))
+            {
+                d[nums[i]]++;
+            }
+            else
+            {
+                d[nums[i]] = 1;
+            }
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (d[nums[i]] > nums.Length / 2)
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    // --------------- O(n)112ms --------------- O(1)27.2MB ---------------
+    /*
+     * Moore Voting 
+     * must have more than half of the numbers
+     */
+    public int MajorityElement_2(int[] nums)
+    {
+        int n=0;
+        int cnt = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (cnt == 0)
+            {
+                n = nums[i];
+                cnt++;
+            }
+            else
+            {
+                if (n == nums[i])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    cnt--;
+                }
+            }
+        }
+        return n;
+    }
+
+}
+/**************************************************************************************************************
+ * MajorityElement_1    MajorityElement_2                                                                     *
+ **************************************************************************************************************/
