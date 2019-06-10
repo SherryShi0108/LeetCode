2bae7383f5a0b699fe179a908b4bd3a2368fff56
XinRuoShi <XinruoShi>
2019-06-10 15:11:24 +0800

    414 485

---
+++ CSharp/Max Consecutive Ones.cs
@@ -0,0 +1,68 @@
+﻿//Source  : https://leetcode.com/problems/max-consecutive-ones/
+//Author  : Xinruo Shi
+//Date    : 2019-06-13
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given a binary array, find the maximum number of consecutive 1s in this array.
+ * 
+ * Note:
+ * The input array will only contain 0 and 1.
+ * The length of input array is a positive integer and will not exceed 10,000
+ * 
+ * Input: nums = [1,1,0,1,1,1]
+ * Output: 3
+ * Explanation: The first two digits or the last three digits are consecutive 1s.The maximum number of consecutive 1s is 3.
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution485
+{
+    // --------------- O(n) 152ms --------------- 33.1MB --------------- (23.6% 5.6%)
+    /*
+     * Attention:if all i cross one-if, what's the return 
+     */
+    public int FindMaxConsecutiveOnes_1(int[] nums)
+    {
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                sum = sum + 1;
+            }
+            else
+            {
+                max = max > sum ? max : sum;
+                sum = 0;
+            }
+        }
+        max = max > sum ? max : sum;
+        return max;
+    }
+
+    // --------------- O(n) 164ms --------------- 33.2MB --------------- (5.6% 5.6%)
+    public int FindMaxConsecutiveOnes_2(int[] nums)
+    {
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 1)
+            {
+                sum++;
+                max = max > sum ? max : sum;
+            }
+            else
+            {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
+/**************************************************************************************************************
+ * FindMaxConsecutiveOnes_2                                                                                   *
+ **************************************************************************************************************/
---
+++ CSharp/Third Maximum Number.cs
@@ -0,0 +1,130 @@
+﻿//Source  : https://leetcode.com/problems/third-maximum-number/
+//Author  : Xinruo Shi
+//Date    : 2019-06-11
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given a non-empty array of integers, return the third maximum number in this array. 
+ * If it does not exist, return the maximum number. The time complexity must be in O(n).
+ * 
+ * Input: nums = [3,2,1]
+ * Output: 1
+ * 
+ * Input: nums = [2, 2, 3, 1]
+ * Output: 1
+ * Explanation: Note that the third maximum here means the third maximum distinct number.
+ *              Both numbers with value 2 are both considered as second maximum.
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution414
+{
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+    /*
+     * integers!!! 
+     * not non-negative
+     */
+    public int ThirdMax_1(int[] nums)
+    {
+        int a = -1;
+        int b = -1;
+        int c = -1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > a)
+            {
+                c = b;
+                b = a;
+                a = nums[i];
+            }
+            if (nums[i] > b && nums[i] < a)
+            {
+                c = b;
+                b = nums[i];
+            }
+            if (nums[i] > c && nums[i] < b)
+            {
+                c = nums[i];
+            }
+        }
+
+        if (c == -1)
+        {
+            return a;
+        }
+        return c;
+    }
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+
+    // --------------- O(n) 100ms --------------- 22.7MB --------------- (48.5% 39.8%)
+    public int ThirdMax_2(int[] nums)
+    {
+        if (nums.Length < 3)
+        {
+            if (nums.Length == 0 || nums == null) { return 0; }
+            if (nums.Length == 1) { return nums[0]; }
+            else { return nums[0] > nums[1] ? nums[0] : nums[1]; }
+        }
+
+        long a = long.MinValue; long b = long.MinValue; long c = long.MinValue;
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > a)
+            {
+                c = b;
+                b = a;
+                a = nums[i];
+            }
+            else if (nums[i] > b && nums[i] < a)
+            {
+                c = b;
+                b = nums[i];
+            }
+            else if (nums[i] > c && nums[i] < b)
+            {
+                c = nums[i];
+            }
+            else
+            {
+                continue;
+            }
+            count++;
+        }
+
+        return count > 2 ? (int)c : (int)a;
+    }
+
+    // --------------- O(n) 152ms --------------- 22.8MB --------------- (5.5% 38.1%)
+    public int ThirdMax_3(int[] nums)
+    {
+        long a = long.MinValue;
+        long b = long.MinValue;
+        long c = long.MinValue;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > a)
+            {
+                c = b;
+                b = a;
+                a = nums[i];
+            }
+            if (nums[i] > b && nums[i] < a)
+            {
+                c = b;
+                b = nums[i];
+            }
+            if (nums[i] > c && nums[i] < b)
+            {
+                c = nums[i];
+            }
+        }
+
+        return c == long.MinValue ? (int)a : (int)c;
+    }
+}
+/**************************************************************************************************************
+ * ThirdMax_3                                                                                                 *
+ **************************************************************************************************************/
