e8a529dd44f374ac9c1332d9bd24d11b127ed46d
XinRuoShi <XinruoShi>
2019-06-13 11:43:20 +0800

    268 717

---
+++ CSharp/1-bit and 2-bit Characters.cs
@@ -0,0 +1,118 @@
+﻿//Source  : https://leetcode.com/problems/1-bit-and-2-bit-characters/
+//Author  : Xinruo Shi
+//Date    : 2019-06-27
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * We have two special characters. The first character can be represented by one bit 0. The second character can be represented by two bits (10 or 11).
+ * Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. The given string will always end with a zero.
+ * 
+ * Note:
+ *  1 <= len(bits) <= 1000.
+ *  bits[i] is always 0 or 1.
+ * 
+ * Input: bits = [1, 0, 0]
+ * Output: True
+ * Explanation: The only way to decode it is two-bit character and one-bit character. So the last character is one-bit character.
+ * 
+ *Input: bits = [1, 1, 1, 0]
+ * Output: False
+ * Explanation: The only way to decode it is two-bit character and two-bit character. So the last character is NOT one-bit character.
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution717
+{
+    // --------------- O(n) 100ms --------------- 22.9MB --------------- (34% 6.7%)
+    public bool IsOneBitCharacter_1(int[] bits)
+    {
+        for (int i = 0; i < bits.Length; i++)
+        {
+            if (bits[i] == 1)
+            {
+                i = i + 1;
+            }
+            else
+            {
+                if (i == bits.Length - 1)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // --------------- O(n) 96ms --------------- 22.7MB --------------- (48% 50%)
+    public bool IsOneBitCharacter_2(int[] bits)
+    {
+        int count = 0;
+        while (count < bits.Length - 1)
+        {
+            if (bits[count] == 1)
+            {
+                count = count + 2;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        return count == bits.Length - 1;
+    }
+
+    // --------------- O(n) 92ms --------------- 22.8MB --------------- (92% 6.7%)
+    public bool IsOneBitCharacter_3(int[] bits)
+    {
+        int count = 0;
+        while (count < bits.Length - 1)
+        {
+            count += bits[count] + 1;
+        }
+        return count == bits.Length - 1;
+    }
+
+    // --------------- O(n) 96ms --------------- 22.8MB --------------- (48% 7%)
+    public bool IsOneBitCharacter_4(int[] bits)
+    {
+        int i = bits.Length - 2;
+        while (i >= 0 && bits[i] > 0)
+        {
+            i--;
+        }
+        return (bits.Length - i) % 2 == 0;
+    }
+
+    // --------------- O(n) 96ms --------------- 22.7MB --------------- (48% 37%)
+    public bool IsOneBitCharacter_5(int[] bits)
+    {
+        int i = bits.Length - 2;
+        for (i = bits.Length - 2; i >=0; i--)
+        {
+            if (bits[i] == 0)
+            {
+                break;
+            }
+        }
+
+        return (bits.Length - i - 1) % 2 != 0;
+        /*
+         * if ((bits.Length - i - 1) % 2 == 0)
+         * {
+         *    return false;
+         * }
+         * else
+         * {
+         *    return true;
+         * }
+         */
+    }
+
+
+   
+}
+
+/**************************************************************************************************************
+ * IsOneBitCharacter_1/2        IsOneBitCharacter_4/5                                                         *
+ **************************************************************************************************************/
---
+++ CSharp/Missing Number.cs
@@ -0,0 +1,118 @@
+﻿//Source  : https://leetcode.com/problems/missing-number/
+//Author  : Xinruo Shi
+//Date    : 2019-06-09
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+ * 
+ * Note:
+ * Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+ * 
+ * Input: nums = [3,0,1]
+ * Output: 2
+ * 
+ * Input: nums = [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ * 
+ *******************************************************************************************************************************/
+
+using System.Collections.Generic;
+
+public class Solution268
+{
+    // --------------- HashTable --------------- O(n) 128ms --------------- 34.3MB --------------- (24% 5%)
+    public int MissingNumber_1(int[] nums)
+    {
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length + 1; i++)
+        {
+            d.Add(i, 1);
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            d.Remove(nums[i]);
+        }
+
+        foreach (var key in d.Keys)
+        {
+            return key;
+        }
+        return -1;
+    }
+
+    // --------------- HashTable --------------- O(n) 172ms --------------- 34.3MB --------------- (5% 5%)
+    public int MissingNumber_2(int[] nums)
+    {
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            d.Add(nums[i], 1);
+        }
+        for (int i = 0; i < nums.Length + 1; i++)
+        {
+            if (!d.ContainsKey(i))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // --------------- O(n) 116ms --------------- 27.9MB --------------- (53% 15%)
+    public int MissingNumber_3(int[] nums)
+    {
+        long sum = 0; long sum2 = 0;
+        for (int i = 0; i < nums.Length + 1; i++)
+        {
+            sum += i;
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum2 += nums[i];
+        }
+
+        return (int)(sum - sum2);
+    }
+
+    // --------------- O(n) 132ms --------------- 27.7MB --------------- (15% 19%)
+    public int MissingNumber_4(int[] nums)
+    {
+        long sum = nums.Length;
+        long sum2 = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += i;
+            sum2 += nums[i];
+        }
+        return (int)(sum - sum2);
+
+    }
+
+    // --------------- Gauss' Formula --------------- O(n) 144ms --------------- 27.6MB --------------- (7% 25%)
+    public int MissingNumber_5(int[] nums)
+    {
+        long sum = nums.Length * (nums.Length + 1) / 2;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum = sum - nums[i];
+        }
+        return (int)sum;
+    }
+
+    // --------------- Bit Manipulation --------------- O(n) 148ms --------------- 27.5MB --------------- (6% 38%)
+    public int MissingNumber_6(int[] nums)
+    {
+        int missing = nums.Length;
+
+        for (int i = 0; i < nums.Length; i++)
+            missing = missing ^ i ^ nums[i];
+
+        return missing;
+    }
+}
+/**************************************************************************************************************
+ *  MissingNumber_1/2 MissingNumber_3/4/5 MissingNumber_6                                                     *
+ **************************************************************************************************************/
