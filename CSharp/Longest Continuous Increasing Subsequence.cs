e32fc27d226903e0a37e81154905137caf7561a1
XinRuoShi <XinruoShi>
2019-06-17 14:49:39 +0800

    88 674

---
+++ CSharp/Longest Continuous Increasing Subsequence.cs
@@ -0,0 +1,90 @@
+﻿//Source  : https://leetcode.com/problems/longest-continuous-increasing-subsequence/
+//Author  : Xinruo Shi
+//Date    : 2019-06-25
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an unsorted array of integers, find the length of longest continuous increasing subsequence (subarray).
+ * 
+ * Note:
+ * Length of the array will not exceed 10,000.
+ * 
+ * Input: nums = [1,3,5,4,7]
+ * Output: 3
+ * Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3. 
+ * Even though [1,3,5,7] is also an increasing subsequence, it's not a continuous one where 5 and 7 are separated by 4. 
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution674
+{
+    // --------------- O(n) 116ms --------------- 23.9MB --------------- (10% 87%)
+    public int FindLengthOfLCIS_1(int[] nums)
+    {
+        if (nums.Length == 0 || nums == null) { return 0; }
+
+        int max = 0;
+        int count = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                count++;
+                max = max > count ? max : count;
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+
+        max = max > count ? max : count;
+        return max;
+    }
+
+    // --------------- O(n) 104ms --------------- 24.3MB --------------- (38% 5%)
+    public int FindLengthOfLCIS_2(int[] nums)
+    {
+        if (nums.Length == 0 || nums == null) { return 0; }
+
+        int max =1;
+        int count = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                count++;
+                max = max > count ? max : count;
+            }
+            else
+            {
+                count = 1;
+            }
+        }    
+        return max;
+    }
+
+    // --------------- O(n) 100ms --------------- 24.3MB --------------- (55% 5%)
+    public int FindLengthOfLCIS_3(int[] nums)
+    {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i==0||nums[i] > nums[i - 1])
+            {
+                count++;
+                max = max > count ? max : count;
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+        return max;
+    }   
+}
+/**************************************************************************************************************
+ * FindLengthOfLCIS_3                                                                                         *
+ **************************************************************************************************************/
---
+++ CSharp/Merge Sorted Array.cs
@@ -0,0 +1,87 @@
+﻿//Source  : https://leetcode.com/problems/merge-sorted-array/
+//Author  : Xinruo Shi
+//Date    : 2019-05-31
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ * 
+ * Notes:
+ * The number of elements initialized in nums1 and nums2 are m and n respectively.
+ * You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+ * 
+ * Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * Output: [1,2,2,3,5,6]
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution88
+{
+    // --------------- O(n) 304ms --------------- 28.3MB --------------- (5% 99%)
+    public void Merge_1(int[] nums1, int m, int[] nums2, int n)
+    {
+        int x = m - 1;
+        int y = n - 1;
+        for (int i = m + n - 1; i >= 0; i--)
+        {
+            if (x >= 0 && y >= 0)
+            {
+                if (nums1[x] > nums2[y])
+                {
+                    nums1[i] = nums1[x];
+                    x--;
+                }
+                else
+                {
+                    nums1[i] = nums2[y];
+                    y--;
+                }
+            }
+            else if (x >= 0)
+            {
+                nums1[i] = nums1[x];
+                x--;
+            }
+            else if (y >= 0)
+            {
+                nums1[i] = nums2[y];
+                y--;
+            }
+        }
+    }
+
+    // --------------- O(n) 248ms --------------- 28.7MB --------------- (80% 24%)
+    /*
+     * Cause nums1 are sorted, so if only x>=0, wo can ignore
+     */
+    public void Merge_2(int[] nums1, int m, int[] nums2, int n)
+    {
+        int x = m - 1;
+        int y = n - 1;
+        for (int i = m + n - 1; i >= 0; i--)
+        {
+            if (x >= 0 && y >= 0)
+            {
+                if (nums1[x] > nums2[y])
+                {
+                    nums1[i] = nums1[x];
+                    x--;
+                }
+                else
+                {
+                    nums1[i] = nums2[y];
+                    y--;
+                }
+            }
+            else if (y >= 0)
+            {
+                nums1[i] = nums2[y];
+                y--;
+            }
+        }
+    }
+}
+/**************************************************************************************************************
+ *   Merge_2                                                                                                  *
+ **************************************************************************************************************/
