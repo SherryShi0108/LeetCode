08ceaf71b148efadef4226bb675a4bd34bc389ff
XinRuoShi <XinruoShi>
2019-06-19 10:16:53 +0800

    283 509

---
+++ CSharp/Fibonacci Number.cs
@@ -0,0 +1,70 @@
+﻿//Source  : https://leetcode.com/problems/fibonacci-number/
+//Author  : Xinruo Shi
+//Date    : 2019-06-14
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+ * such that each number is the sum of the two preceding ones, starting from 0 and 1. That is, Given N, calculate F(N).
+ *          F(0) = 0,   F(1) = 1
+ *          F(N) = F(N - 1) + F(N - 2), for N > 1.
+ * 
+ * Note:
+ * 0 ≤ N ≤ 30.
+ * 
+ * Input: 2
+ * Output: 1
+ * Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution509
+{
+    // --------------- O(n) 36ms --------------- 12.7MB --------------- (99.3% 99.4%)
+    public int Fib_1(int N)
+    {
+        if (N < 2) { return N; }
+
+        int[] a = new int[N];
+        a[0] = 0; a[1] = 1;
+        for (int i = 2; i < N; i++)
+        {
+            a[i] = a[i - 1] + a[i - 2];
+        }
+
+        return a[N - 1] + a[N - 2];
+    }
+
+    // --------------- O(n) 36ms --------------- 12.7MB --------------- (99.3% 99.4%)
+    public int Fib_2(int N)
+    {
+        if (N == 0) { return 0; }
+
+        int a = 0; int b = 1;
+        for (int i = 2; i < N; i++)
+        {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return a + b;
+    }
+
+    // --------------- O(n) 40ms --------------- 12.6MB --------------- (63% 100%)
+    /*
+     * But really, repeatedly recalculating unchanging constants is stupid.
+     * Do you start summing infinite series to figure out the value of pi every time you want the area of a circle?
+     * Of course not! The work is already done, and you just use the pre-calculated constant. It's the same idea here.
+     */
+    static readonly int[] Fibs = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584,
+        4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040 };
+    public int Fib_3(int N)
+    {
+        return Fibs[N];
+    }
+}
+/**************************************************************************************************************
+ * Fib_1 Fib_2                                                                                                *
+ **************************************************************************************************************/
---
+++ CSharp/Move Zeroes.cs
@@ -0,0 +1,102 @@
+﻿//Source  : https://leetcode.com/problems/move-zeroes/
+//Author  : Xinruo Shi
+//Date    : 2019-06-10
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * 
+ * Note:
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ * 
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution283
+{
+    // --------------- O(n) 264ms --------------- 29.8MB --------------- (40% 95%)
+    public void MoveZeroes_1(int[] nums)
+    {
+        int count0 = 0;
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0)
+            {
+                count0++;
+            }
+            else
+            {
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+
+        while (count0 > 0)
+        {
+            nums[nums.Length - count0] = 0;
+            count0--;
+        }
+    }
+
+    // --------------- O(n) 252ms --------------- 30MB --------------- (75% 57%)
+    public void MoveZeroes_2(int[] nums)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[count++] = nums[i];
+            }
+        }
+        while (count < nums.Length)
+        {
+            nums[count] = 0;
+            count++;
+        }
+    }
+
+    // --------------- O(n) 260ms --------------- 30MB --------------- (50% 57%)
+    public void MoveZeroes_3(int[] nums)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[count] = nums[i];
+                if (i > count)
+                {
+                    nums[i] = 0;
+                }
+
+                count++;
+            }
+        }
+    }
+
+    // --------------- O(n) 256ms --------------- 30MB --------------- (60% 57%)
+    public void MoveZeroes_4(int[] nums)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                int temp = nums[count];
+                nums[count] = nums[i];
+                nums[i] = temp;
+
+                count++;
+            }
+        }
+    }
+}
+/**************************************************************************************************************
+ * MoveZeroes_2 MoveZeroes_3 MoveZeroes_4                                                                     *
+ **************************************************************************************************************/
