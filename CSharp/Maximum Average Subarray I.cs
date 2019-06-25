8b89c76fbc9db9d483e9ab60a2d00371a548c12b
XinRuoShi <XinruoShi>
2019-06-25 11:06:34 +0800

    628 643

---
+++ CSharp/Maximum Average Subarray I.cs
@@ -0,0 +1,89 @@
+﻿//Source  : https://leetcode.com/problems/maximum-average-subarray-i/
+//Author  : Xinruo Shi
+//Date    : 2019-06-22
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value. 
+ * And you need to output the maximum average value.
+ * 
+ * Note:
+ * 1 <= k <= n <= 30,000.
+ * Elements of the given array will be in the range [-10,000, 10,000].
+ * 
+ * Input: nums = [1,12,-5,-6,50,3], k = 4
+ * Output: 12.75
+ * Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution643
+{
+    // --------------- O(n) 264ms --------------- 42.4MB --------------- (41% 6%)
+    public double FindMaxAverage_1(int[] nums, int k)
+    {
+        if (nums.Length == 0 || nums == null) { return 0; }
+
+        double max = double.MinValue;
+        double n = 0;
+        int temp = k;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            while (k > 0)
+            {
+                n += nums[i];
+                k--;
+                i++;      
+            }
+            max = max > n ? max : n;
+
+            if (i<nums.Length)
+            {
+                n = n + nums[i] - nums[i - temp];
+                max = max > n ? max : n;
+            }
+        }
+        return max/temp;
+    }
+
+    // --------------- O(n) 280ms --------------- 42.3MB --------------- (15% 6%)
+    public double FindMaxAverage_2(int[] nums, int k)
+    {
+        double sum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            sum += nums[i];
+        }
+        double max = sum;
+        for (int i = k; i < nums.Length; i++)
+        {
+            sum = sum + nums[i] - nums[i - k];
+            max = max > sum ? max : sum;
+        }
+        return max/k;
+    }
+
+    // --------------- O(n) 256ms --------------- 42.4MB --------------- (58% 6%) ※
+    /*
+     * var instead double, time will less than
+     */
+    public double FindMaxAverage_3(int[] nums, int k)
+    {
+        var sum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            sum += nums[i];
+        }
+        var max = sum;
+        for (int i = k; i < nums.Length; i++)
+        {
+            sum = sum + nums[i] - nums[i - k];
+            max = max > sum ? max : sum;
+        }
+        return (double)max / k;
+    }
+}
+/**************************************************************************************************************
+ * FindMaxAverage_3                                                                                           *
+ **************************************************************************************************************/
---
+++ CSharp/Maximum Product of Three Numbers.cs
@@ -0,0 +1,115 @@
+﻿//Source  : https://leetcode.com/problems/maximum-product-of-three-numbers/
+//Author  : Xinruo Shi
+//Date    : 2019-06-21
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an integer array, find three numbers whose product is maximum and output the maximum product.
+ * 
+ * Note:
+ * The length of the given array will be in range [3,10^4] and all elements are in the range [-1000, 1000].
+ * Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
+ * 
+ * Input: nums = [1,2,3]
+ * Output: 6
+ * 
+ * Input: nums = [1,2,3,4]
+ * Output: 24
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution628
+{
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+    /*
+     *     " - * - = + "
+     * if input: [-4,-3,-2,-1,60] 
+     * this method is -2 * -1 * 60 = 120, but should be -4 * -3 * 60 = 720
+     */
+    public int MaximumProduct_1(int[] nums)
+    {
+        int[] d = new int[3] { int.MinValue, int.MinValue, int.MinValue };
+        d[0] = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] >= d[0])
+            {
+                d[2] = d[1];
+                d[1] = d[0];
+                d[0] = nums[i];
+            }
+            else if (nums[i] < d[0] && nums[i] >= d[1])
+            {
+                d[2] = d[1];
+                d[1] = nums[i];
+            }
+            else if (nums[i] > d[2] && nums[i] < d[1])
+            {
+                d[2] = nums[i];
+            }
+
+        }
+
+        return d[0] * d[1] * d[2];
+    }
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+
+    // --------------- O(n) 144ms --------------- 32.5MB --------------- (70% 6%) ※
+    public int MaximumProduct_2(int[] nums)
+    {
+        int[] d1 = new int[3] { int.MinValue, int.MinValue, int.MinValue };
+        int[] d2 = new int[2] { int.MaxValue, int.MaxValue };
+
+        d1[0] = nums[0];
+        d2[0] = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] >= d1[0])
+            {
+                d1[2] = d1[1];
+                d1[1] = d1[0];
+                d1[0] = nums[i];
+            }
+            else if (nums[i] < d1[0] && nums[i] >= d1[1])
+            {
+                d1[2] = d1[1];
+                d1[1] = nums[i];
+            }
+            else if (nums[i] > d1[2] && nums[i] < d1[1])
+            {
+                d1[2] = nums[i];
+            }
+
+            if (nums[i] < d2[0])
+            {
+                d2[1] = d2[0];
+                d2[0] = nums[i];
+            }
+            else if (nums[i] >= d2[0] && nums[i] < d2[1])
+            {
+                d2[1] = nums[i];
+            }
+        }
+
+        int max = (d1[0] * d1[1] * d1[2]) > (d1[0] * d2[1] * d2[0]) ? (d1[0] * d1[1] * d1[2]) : (d1[0] * d2[1] * d2[0]);
+
+        return max;
+    }
+
+    // --------------- O(nlogn) 164ms --------------- 32.5MB --------------- (32% 6%)
+    /*
+     * sort
+     */
+    public int MaximumProduct_3(int[] nums)
+    {
+        System.Array.Sort(nums);
+
+        int n = nums.Length;
+        int max = nums[n - 1] * nums[n - 2] * nums[n - 3] > nums[n - 1] * nums[0] * nums[1] ? nums[n - 1] * nums[n - 2] * nums[n - 3] : nums[n - 1] * nums[0] * nums[1];
+        return max;
+    }
+}
+/**************************************************************************************************************
+ * MaximumProduct_2                                                                                           *
+ **************************************************************************************************************/
