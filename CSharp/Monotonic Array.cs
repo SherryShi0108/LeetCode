5b2720124e27d4f464c359259c43bc9e49532ded
XinRuoShi <XinruoShi>
2019-06-27 10:53:58 +0800

    896 914

---
+++ CSharp/Monotonic Array.cs
@@ -0,0 +1,133 @@
+﻿//Source  : https://leetcode.com/problems/monotonic-array/
+//Author  : Xinruo Shi
+//Date    : 2019-07-08
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * An array is monotonic if it is either monotone increasing or monotone decreasing.
+ * An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A is monotone decreasing if for all i <= j, A[i] >= A[j].
+ * Return true if and only if the given array A is monotonic.
+ * 
+ * Note:
+ * 1 <= A.length <= 50000
+ * -100000 <= A[i] <= 100000
+ * 
+ * Input: [1,2,2,3]
+ * Output: true
+ * 
+ * Input: [6,5,4,4]
+ * Output: true
+ * 
+ * Input: [1,3,2]
+ * Output: false
+ * 
+ * Input: [1,2,4,5]
+ * Output: true
+ * 
+ * Input: [1,1,1]
+ * Output: true
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution896
+{
+    // --------------- O(n) 184ms --------------- 38MB --------------- (40% 11%)
+    public bool IsMonotonic_1(int[] A)
+    {
+        int flag = 0;
+        int flag1 = 0;
+        int flag2 = 0;
+
+        for (int i = 1; i < A.Length; i++)
+        {
+            if (A[i] == A[i - 1])
+            {
+                flag++;
+            }
+            else if (A[i] > A[i - 1])
+            {
+                flag1++;
+            }
+            else if (A[i] < A[i - 1])
+            {
+                flag2++;
+            }
+        }
+
+        if (flag + flag1 == A.Length - 1 || flag + flag2 == A.Length - 1)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // --------------- O(n) 172ms --------------- 37.9MB --------------- (70% 13%) 
+    public bool IsMonotonic_2(int[] A)
+    {
+        return Increasing(A) || Decreasing(A);
+    }
+    public bool Increasing(int[] A)
+    {
+        for (int i = 1; i < A.Length; i++)
+        {
+            if (A[i] < A[i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public bool Decreasing(int[] A)
+    {
+        for (int i = 1; i < A.Length; i++)
+        {
+            if (A[i] > A[i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // --------------- O(n) 176ms --------------- 37.8MB --------------- (61% 29%) ※
+    public bool IsMonotonic_3(int[] A)
+    {
+        bool increasing = true;
+        bool decreasing = true;
+        for (int i = 1; i < A.Length; i++)
+        {
+            if (A[i] > A[i - 1])
+            {
+                decreasing = false;
+            }
+            if (A[i] < A[i - 1])
+            {
+                increasing = false;
+            }
+        }
+        return decreasing || increasing;
+    }
+
+    // --------------- O(n) 196ms --------------- 37.7MB --------------- (11% 41%)
+    public bool IsMonotonic_4(int[] A)
+    {
+        bool increasing = true;
+        bool decreasing = true;
+        for (int i = 1; i < A.Length; i++)
+        {
+            if (increasing)
+            {
+                increasing = increasing && (A[i] >= A[i - 1]);
+            }
+            if (decreasing)
+            {
+                decreasing = decreasing && (A[i] <= A[i - 1]);
+            }
+        }
+        return increasing || decreasing;
+    }
+}
+/**************************************************************************************************************
+ * IsMonotonic_1 IsMonotonic_2 IsMonotonic_3 IsMonotonic_4                                                    *
+ **************************************************************************************************************/
---
+++ CSharp/X of a Kind in a Deck of Cards.cs
@@ -0,0 +1,119 @@
+﻿//Source  : https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/
+//Author  : Xinruo Shi
+//Date    : 2019-07-12
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * In a deck of cards, each card has an integer written on it.
+ * Return true if and only if you can choose X >= 2 such that it is possible to split the entire deck into 1 or more groups of cards, 
+ * where: Each group has exactly X cards. All the cards in each group have the same integer.
+ * 
+ * Note:
+ * 1 <= deck.length <= 10000
+ * 0 <= deck[i] < 10000
+ * 
+ * Input: [1,2,3,4,4,3,2,1]
+ * Output: true
+ * Explanation: Possible partition [1,1],[2,2],[3,3],[4,4]
+ * 
+ * Input: [1,1,1,2,2,2,3,3]
+ * Output: false
+ * Explanation: No possible partition.
+ * 
+ * Input: [1]
+ * Output: false
+ * Explanation: No possible partition.
+ * 
+ * Input: [1,1]
+ * Output: true
+ * Explanation: Possible partition [1,1]
+ * 
+ * Input: [1,1,2,2,2,2]
+ * Output: true
+ * Explanation: Possible partition [1,1],[2,2],[2,2]
+ * 
+ *******************************************************************************************************************************/
+
+using System.Collections.Generic; //Dictionary名称空间
+
+public class Solution914
+{
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+    /*
+     *     // [0,0,0,1,1,1,2,2,2] should be true
+     *     X >= 2 !!! not X = 2 !!!
+     *     You need to read the topic carefully!
+     */
+    public bool HasGroupsSizeX_1(int[] deck)
+    {
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        for (int i = 0; i < deck.Length; i++)
+        {
+            if (d.ContainsKey(deck[i]))
+            {
+                d.Remove(deck[i]);
+            }
+            else
+            {
+                d[deck[i]] = 1;
+            }
+        }
+
+        if (d.Count > 0)
+        {
+            return false;
+        }
+        return true;
+    }
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+
+    // --------------- O(n^2) 140ms --------------- 26.5MB --------------- (10% 60%)
+    public bool HasGroupsSizeX_2(int[] deck)
+    {
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        for (int i = 0; i < deck.Length; i++)
+        {
+            if (d.ContainsKey(deck[i]))
+            {
+                d[deck[i]]++;
+            }
+            else
+            {
+                d[deck[i]] = 1;
+            }
+        }
+
+        int temp = deck.Length;
+        for (int i = 2; i < deck.Length; i++)
+        {
+            bool b = true;
+            foreach (var item in d)
+            {
+                if (item.Value % i != 0)
+                {
+                    b = false;
+                }
+            }
+            if (b)
+            {
+                temp = i;
+                break;
+            }
+        }
+
+        bool flag = true;
+        foreach (var item in d)
+        {
+            if (item.Value % temp != 0 || temp == 1)
+            {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
+/**************************************************************************************************************
+ * HasGroupsSizeX_2                                                                                           *
+ **************************************************************************************************************/
