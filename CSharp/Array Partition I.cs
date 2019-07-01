0a2759de7dab737329c07052c92236da86e6da58
XinRuoShi <XinruoShi>
2019-07-01 16:56:18 +0800

    561 697

---
+++ CSharp/Array Partition I.cs
@@ -0,0 +1,110 @@
+﻿//Source  : https://leetcode.com/problems/array-partition-i/
+//Author  : Xinruo Shi
+//Date    : 2019-06-16
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array of 2n integers, your task is to group these integers into n pairs of integer, 
+ * say (a1, b1), (a2, b2), ..., (an, bn) which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+ * 
+ * Note:
+ * n is a positive integer, which is in the range of [1, 10000].
+ * All the integers in the array will be in the range of [-10000, 10000].
+ * 
+ * Input: nums = [1,4,3,2]
+ * Output: 4
+ * Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
+ * 
+ *******************************************************************************************************************************/
+
+using System;
+
+public class Solution561
+{
+    // --------------- max[ O(n), O(Sort) ] 180ms --------------- 33.1MB --------------- (27% 21%)
+    /*
+     * use Array.Sort 
+     */
+    public int ArrayPairSum_1(int[] nums)
+    {
+        Array.Sort(nums);
+
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i += 2)
+        {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+
+    // --------------- O(n) 164ms --------------- 39.7MB --------------- (64% 5%)
+    public int ArrarPairSum_2(int[] nums)
+    {
+        int[] n = new int[20001];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            n[nums[i] + 10000]++;
+        }
+
+        int sum = 0;
+        bool flag = true;
+        for (int i = 0; i < n.Length; i++)
+        {
+            while (n[i] > 0)
+            {
+                if (flag)
+                {
+                    sum += i - 10000;
+                }
+                flag = !flag;
+                n[i]--;
+            }
+        }
+
+        return sum;
+    }
+
+    // --------------- O(n) 144ms --------------- 37MB --------------- (98% 6%) 
+    /*
+     * cannot understand ??????????
+     */
+    public int ArrarPairSum_3(int[] nums)
+    {
+        int min = int.MaxValue, max = int.MinValue;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            min = nums[i] < min ? nums[i] : min;
+            max = nums[i] > max ? nums[i] : max;
+        }
+
+        int[] counts = new int[max - min + 1];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            counts[nums[i] - min]++;
+        }
+
+        int k = 0;
+
+        for (int j = max; j >= min; j--)
+        {
+            for (int i = 0; i < counts[j - min]; i++)
+            {
+                nums[k++] = j;
+            }
+        }
+        var sum = 0;
+        for (var t = nums.Length - 1; t >= 0; t = t - 2)
+        {
+            sum += Math.Min(nums[t], nums[t - 1]);
+        }
+
+        return sum;
+    }
+}
+/**************************************************************************************************************
+ * ArrayPairSum_1 ArrayPairSum_2                                                                              *
+ **************************************************************************************************************/
---
+++ CSharp/Degree of an Array.cs
@@ -0,0 +1,103 @@
+﻿//Source  : https://leetcode.com/problems/degree-of-an-array/
+//Author  : Xinruo Shi
+//Date    : 2019-06-26
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
+ * Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
+ * 
+ * Note:
+ * nums.length will be between 1 and 50,000.
+ * nums[i] will be an integer between 0 and 49,999.
+ * 
+ * Input: nums = [1, 2, 2, 3, 1]
+ * Output: 2
+ * Explanation: The input array has a degree of 2 because both elements 1 and 2 appear twice.Of the subarrays that have the same degree:
+ * [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2] The shortest length is 2. So return 2.
+ * 
+ * Input: nums = [1,2,2,3,1,4,2]
+ * Output: 6
+ * 
+ *******************************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+public class Solution697
+{
+    // --------------- O(n) 176ms --------------- 30.2MB --------------- (19% 50%)
+    /*
+     * int[] 0:出现次数 1：这个数字出现的第一个位置 2：最新位置到第一个位置之间举例
+     */
+    public int FindShortestSubArray_1(int[] nums)
+    {
+        Dictionary<int, int[]> d = new Dictionary<int, int[]>();
+
+        int max = 1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (d.ContainsKey(nums[i]))
+            {
+                int[] a = d[nums[i]];
+                a[0]++;
+                a[2] = i - a[1];
+
+                max = a[0] > max ? a[0] : max;
+            }
+            else
+            {
+                d[nums[i]] = new int[] { 1, i, 0 };
+            }
+        }
+
+        int min = int.MaxValue;
+        foreach (var item in d)
+        {
+            int[] a = item.Value;
+            if (a[0] == max)
+            {
+                min = min < a[2] ? min : a[2];
+            }
+        }
+
+        return min + 1;
+    }
+
+    // --------------- O(n) 132ms --------------- 30.1MB --------------- (86% 54%) ※
+    public int FindShortestSubArray_2(int[] nums)
+    {
+        Dictionary<int, int[]> d = new Dictionary<int, int[]>();
+        int max = 0;
+        int min = 1;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!d.ContainsKey(nums[i]))
+            {
+                d[nums[i]] = new int[] { 1, i, 0 };
+            }
+            else
+            {
+                int[] a = d[nums[i]];
+                a[0]++;
+                a[2] = i - a[1];
+                
+                if (a[0] > max)
+                {
+                    max = a[0];
+                    min = a[2] + 1;
+                }
+                else if (a[0]==max)
+                {
+                    min = min < a[2] + 1 ? min : a[2] + 1;
+                }
+            }
+        }
+        return min;
+    }   
+}
+/**************************************************************************************************************
+ * FindShortestSubArray_2                                                                                     *
+ **************************************************************************************************************/
