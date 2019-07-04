71ed13c9c72f5c37294f81287131af7a8e5764df
XinRuoShi <XinruoShi>
2019-07-04 14:42:12 +0800

    605 747

---
+++ CSharp/Can Place Flowers.cs
@@ -0,0 +1,174 @@
+﻿//Source  : https://leetcode.com/problems/can-place-flowers/
+//Author  : Xinruo Shi
+//Date    : 2019-06-19
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Suppose you have a long flowerbed in which some of the plots are planted and some are not. 
+ * However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
+ * Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), 
+ * and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
+ * 
+ * Note:
+ * The input array won't violate no-adjacent-flowers rule.
+ * The input array size is in the range of [1, 20000].
+ * n is a non-negative integer which won't exceed the input array size.
+ * 
+ * Input: nums = [1,0,0,0,1], n = 1
+ * Output: True
+ *  
+ * Input: nums = [1,0,0,0,1], n = 2
+ * Output: False
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution605
+{
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+    /*
+     * Runtime Error [0]
+     * Index was outside the bounds of the array.
+     */
+    public bool CanPlaceFlowers_1(int[] flowerbed, int n)
+    {
+        int i = 0;
+        int count = 0;
+        while (i < flowerbed.Length)
+        {
+            if (flowerbed[i] == 0)
+            {
+                if (i == 0 && flowerbed[i + 1] == 0)
+                {
+                    count++;
+                    flowerbed[i] = 1;
+                }
+                else if (i == flowerbed.Length - 1 && flowerbed[i - 1] == 0)
+                {
+                    count++;
+                    flowerbed[i] = 1;
+                }
+                else if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)
+                {
+                    count++;
+                    flowerbed[i] = 1;
+                }
+            }
+            i++;
+        }
+
+        return count >= n;
+    }
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+
+    // --------------- O(n) 104ms --------------- 28MB --------------- (99.2% 6%)
+    public bool CanPlaceFlowers_2(int[] flowerbed, int n)
+    {
+        int i = 0;
+        int count = 0;
+        while (i < flowerbed.Length)
+        {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+            {
+                count++;
+                flowerbed[i] = 1;
+            }
+            i++;
+        }
+
+        return count >= n;
+    }
+
+    // --------------- O(n) 100ms --------------- 28MB --------------- (100%)
+    public bool CanPlaceFlowers_3(int[] flowerbed, int n)
+    {
+        int i = 0;
+        int count = 0;
+        while (i < flowerbed.Length)
+        {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+            {
+                count++;
+                flowerbed[i] = 1;
+
+                i += 2;
+            }
+            else
+            {
+                i++;
+            }
+            if (count >= n)
+            {
+                return true;
+            }
+        }
+
+        return count >= n;
+    }
+
+    // --------------- O(n) 120ms --------------- 27.8MB --------------- (34% 13%)
+    /*
+     * easy-understanding
+     */
+    public bool CanPlaceFlowers_4(int[] flowerbed, int n)
+    {
+        int count = 1;
+        int result = 0;
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+            if (flowerbed[i] == 0)
+            {
+                count++;
+            }
+            else
+            {
+                result += (count - 1) / 2;
+                count = 0;
+            }
+        }
+        if (count != 0)
+        {
+            result += count / 2;
+        }
+
+        return result >= n;
+    }
+
+    // --------------- O(n) 120ms --------------- 28MB --------------- (34% 6%)
+    /*
+     *  !!! The input array won't violate no-adjacent-flowers rule. !!!
+     *  means 1 will not near 1 !!!
+     */
+    public bool CanPlaceFlowers_5(int[] flowerbed, int n)
+    {
+        int capacity = 0;
+
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+            if (flowerbed[i] == 1)
+            {
+                ++i;
+                continue;
+            }
+            else
+            {
+                if (i == flowerbed.Length - 1)
+                {
+                    capacity++;
+                    break;
+                }
+                if (flowerbed[i + 1] == 0)
+                {
+                    capacity++;
+                    ++i;
+                    continue;
+                }
+            }
+
+        }
+        return capacity >= n;
+    }
+}
+/**************************************************************************************************************
+ * CanPlaceFlowers_2 CanPlaceFlowers_4 CanPlaceFlowers_5                                                      *
+ **************************************************************************************************************/
---
+++ CSharp/Largest Number At Least Twice of Others.cs
@@ -0,0 +1,103 @@
+﻿//Source  : https://leetcode.com/problems/largest-number-at-least-twice-of-others/
+//Author  : Xinruo Shi
+//Date    : 2019-06-30
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * In a given integer array nums, there is always exactly one largest element.
+ * Find whether the largest element in the array is at least twice as much as every other number in the array.
+ * If it is, return the index of the largest element, otherwise return -1.
+ * 
+ * Note:
+ * nums will have a length in the range [1, 50].
+ * Every nums[i] will be an integer in the range [0, 99].
+ * 
+ * Input: nums = [3, 6, 1, 0]
+ * Output: 1
+ * Explanation: 6 is the largest integer, and for every other number in the array x,6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+ * 
+ * Input: nums = [1, 2, 3, 4]
+ * Output: -1
+ * Explanation: 4 isn't at least as big as twice the value of 3, so we return -1.
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution747
+{
+    // --------------- O(n) 92ms --------------- 22.7MB --------------- (93% 6%) ※
+    public int DominantIndex_1(int[] nums)
+    {
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            max = max > nums[i] ? max : nums[i];
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (max == nums[i])
+            {
+                index = i;
+            }
+            else if (max < 2 * nums[i])
+            {
+                return -1;
+            }
+        }
+        return index;
+    }
+
+    // --------------- O(n) 112ms --------------- 22.7MB --------------- (12% 6%)
+    /*
+     *  1 for
+     */
+    public int DominantIndex_2(int[] nums)
+    {
+        int max1 = 0;
+        int max2 = 0;
+        int maxIndex = -1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > max1)
+            {
+                max2 = max1;
+                max1 = nums[i];
+                maxIndex = i;
+            }
+            else if (nums[i] > max2)
+            {
+                max2 = nums[i];
+            }
+        }
+        if (max1 >= max2 * 2)
+        {
+            return maxIndex;
+        }
+        return -1;
+    }
+
+    // --------------- O(n) 112ms --------------- 22.6MB --------------- (12% 21%)
+    public int DominantIndex_3(int[] nums)
+    {
+        int index = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[index])
+            {
+                index = i;
+            }
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[index] < 2 * nums[i] && index != i)
+            {
+                return -1;
+            }
+        }
+        return index;
+    }
+}
+/**************************************************************************************************************
+ * DominantIndex_1/3 DominantIndex_2                                                                          *
+ **************************************************************************************************************/
