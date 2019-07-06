3504509296c21bb55224393ed35ba5e67a38cd4e
XinRuoShi <XinruoShi>
2019-07-06 11:17:56 +0800

    941 977

---
+++ CSharp/Squares of a Sorted Array.cs
@@ -0,0 +1,92 @@
+﻿//Source  : https://leetcode.com/problems/squares-of-a-sorted-array/
+//Author  : Xinruo Shi
+//Date    : 2019-07-18
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+ * 
+ * Note:
+ * 1 <= A.length <= 10000
+ * -10000 <= A[i] <= 10000
+ * A is sorted in non-decreasing order.
+ * 
+ * Input: [-4,-1,0,3,10]
+ * Output: [0,1,9,16,100]
+ * 
+ * Input: [-7,-3,2,3,11]
+ * Output: [4,9,9,49,121]
+ * 
+ *******************************************************************************************************************************/
+
+using System;
+
+public class Solution977
+{
+    // --------------- O(n+sort) 336ms --------------- 39.9MB --------------- (29% 81%) 
+    public int[] SortedSquares_1(int[] A)
+    {
+        for (int i = 0; i < A.Length; i++)
+        {
+            A[i] = A[i] * A[i];
+        }
+        Array.Sort(A);
+
+        return A;
+    }
+
+    // --------------- O(n) 344ms --------------- 40.4MB --------------- (20% 29%) 
+    public int[] SortedSquares_2(int[] A)
+    {
+        if (A.Length < 1) { return A; }
+
+        int[] B = new int[A.Length];
+        int L = 0;
+        int R = A.Length - 1;
+
+        int i = R;
+        while (L <= R)
+        {
+            if (A[L] * A[L] > A[R] * A[R])
+            {
+                B[i] = A[L] * A[L];
+                L++;
+            }
+            else
+            {
+                B[i] = A[R] * A[R];
+                R--;
+            }
+            i--;
+        }
+
+        return B;
+    }
+
+    // --------------- O(n) 304ms --------------- 40.4MB --------------- (84% 29%) ※
+    public int[] SortedSquares_3(int[] A)
+    {
+        int[] B = new int[A.Length];
+        int L = 0;
+        int R = A.Length - 1;
+
+        for (int i = A.Length - 1; i >= 0; i--)
+        {
+            if (A[L] * A[L] > A[R] * A[R])
+            {
+                B[i] = A[L] * A[L];
+                L++;
+            }
+            else
+            {
+                B[i] = A[R] * A[R];
+                R--;
+            }
+        }
+        return B;
+    }
+}
+/**************************************************************************************************************
+ * SortedSquares_3                                                                                            *
+ **************************************************************************************************************/
---
+++ CSharp/Valid Mountain Array.cs
@@ -0,0 +1,148 @@
+﻿//Source  : https://leetcode.com/problems/valid-mountain-array/
+//Author  : Xinruo Shi
+//Date    : 2019-07-16
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array A of integers, return true if and only if it is a valid mountain array.
+ * Recall that A is a mountain array if and only if:
+ *      A.length >= 3
+ *      There exists some i with 0 < i < A.length - 1 such that: A[0] < A[1] < ... A[i-1] < A[i]
+ *                                                               A[i] > A[i+1] > ... > A[A.length - 1]
+ * Note:
+ * 0 <= A.length <= 10000
+ * 0 <= A[i] <= 10000 
+ * 
+ * Input: [2,1]
+ * Output: false
+ * 
+ * Input: [3,5,5]
+ * Output: false
+ * 
+ * Input: [0,3,2,1]
+ * Output: true
+ *  
+ *******************************************************************************************************************************/
+
+public class Solution941
+{
+    // --------------- O(n) 144ms --------------- 31MB --------------- (12% 5%) 
+    public bool ValidMountainArray_1(int[] A)
+    {
+        if (A.Length < 3) { return false; }
+
+        int temp = 0;
+        for (int i = 0; i < A.Length - 1; i++)
+        {
+            if (A[i] < A[i + 1])
+            {
+                temp++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (temp == 0 || temp == A.Length - 1)
+        {
+            return false;
+        }
+        for (int i = temp; i < A.Length - 1; i++)
+        {
+            if (A[i] > A[i + 1])
+            {
+                temp++;
+            }
+        }
+
+        return temp == A.Length - 1;
+    }
+
+    // --------------- O(n) 140ms --------------- 31MB --------------- (22% 5%) 
+    /*
+     * similar to "1"
+     */
+    public bool ValidMountainArray_2(int[] A)
+    {
+        int temp = 0;
+        while (temp < A.Length - 1 && A[temp] < A[temp + 1])
+        {
+            temp++;
+        }
+        if (temp == 0 || temp == A.Length - 1) return false;
+
+        for (int i = temp; i < A.Length - 1; i++)
+        {
+            if (A[i] <= A[i + 1]) return false;
+        }
+
+        return true;
+    }
+
+    // --------------- O(n) 132ms --------------- 30.9MB --------------- (37% 30%) ※
+    /*
+     *  easy - understanding
+     */
+    public bool ValidMountainArray_3(int[] A)
+    {
+        int i = 0;
+        int j = A.Length - 1;
+
+        while (i < A.Length - 1 && A[i] < A[i + 1])
+        {
+            i++;
+        }
+        while (j > 0 && A[j - 1] > A[j])
+        {
+            j--;
+        }
+
+        return i > 0 && i == j && j < A.Length - 1;
+    }
+
+    // --------------- O(n) 144ms --------------- 31MB --------------- (12% 5%) 
+    /*
+     *  easy - understanding
+     */
+    public bool ValidMountainArray_4(int[] A)
+    {
+        if (A == null || A.Length < 3) return false;
+
+        bool increaseFound = false;
+        bool decreaseFound = false;
+        bool flag = true;
+
+        for (int i = 0; i < A.Length - 1; i++)
+        {
+            if (A[i] == A[i + 1])
+            {
+                return false;
+            }
+
+            if (flag)
+            {
+                if (A[i] < A[i + 1])
+                {
+                    increaseFound = true;
+                }
+                else
+                {
+                    flag = false;
+                    decreaseFound = true;
+                }
+            }
+            else
+            {
+                if (A[i] < A[i + 1])
+                {
+                    return false;
+                }
+            }
+        }
+        return increaseFound && decreaseFound;
+    }    
+}
+/**************************************************************************************************************
+ * ValidMountainArray_2   ValidMountainArray_3   ValidMountainArray_4                                         *
+ **************************************************************************************************************/
