422f14a21e0ac7bfe4a9d897237fe0e14c035b46
XinRuoShi <XinruoShi>
2019-07-12 11:59:36 +0800

    989 999

---
+++ CSharp/Add to Array-Form of Integer.cs
@@ -0,0 +1,172 @@
+﻿//Source  : https://leetcode.com/problems/add-to-array-form-of-integer/
+//Author  : Xinruo Shi
+//Date    : 2019-07-24
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * For a non-negative integer X, the array-form of X is an array of its digits in left to right order.  For example, if X = 1231, then the array form is [1,2,3,1].
+ * Given the array-form A of a non-negative integer X, return the array-form of the integer X+K.
+ * 
+ * Note:
+ * 1 <= A.length <= 10000
+ * 0 <= A[i] <= 9
+ * 0 <= K <= 10000
+ * If A.length > 1, then A[0] != 0
+ * 
+ * Input: A = [1,2,0,0], K = 34
+ * Output: [1,2,3,4]
+ * Explanation: 1200 + 34 = 1234
+ * 
+ * Input: A = [2,7,4], K = 181
+ * Output: [4,5,5]
+ * Explanation: 274 + 181 = 455
+ * 
+ * Input: A = [2,1,5], K = 806
+ * Output: [1,0,2,1]
+ * Explanation: 215 + 806 = 1021
+ * 
+ * Input: A = [9,9,9,9,9,9,9,9,9,9], K = 1
+ * Output: [1,0,0,0,0,0,0,0,0,0,0]
+ * Explanation: 9999999999 + 1 = 10000000000
+ *  
+ *******************************************************************************************************************************/
+
+using System.Collections.Generic;
+
+public class Solution989
+{
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+    /*
+     * it has input cause sum > int.maxValue
+     */
+    public IList<int> AddToArrayForm_1(int[] A, int K)
+    {
+        int sum = 0;
+        for (int i = 0; i < A.Length; i++)
+        {
+            sum = sum * 10 + A[i];
+        }
+
+        sum += K;
+
+        //求sum位数
+        int j = 0; int temp = sum;
+        while (temp > 0)
+        {
+            temp = temp / 10;
+            j++;
+        }
+
+        IList<int> d = new int[j];
+        for (int i = j - 1; i >= 0; i--)
+        {
+            d[i] = sum % 10;
+            sum = sum / 10;
+        }
+
+        return d;
+    }
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+    /*
+     * maybe cause sum > long.maxValue (>double.maxValue)
+     */
+    public IList<int> AddToArrayForm_2(int[] A, int K)
+    {
+        long sum = 0;
+        for (int i = 0; i < A.Length; i++)
+        {
+            sum = sum * 10 + A[i];
+        }
+
+        sum += K;
+
+        //求sum位数
+        int j = 0;
+        long temp = sum;
+        if (temp == 0)
+        {
+            return new int[] { 0 };
+        }
+        while (temp > 0)
+        {
+            temp = temp / 10;
+            j++;
+        }
+
+        IList<int> d = new int[j];
+        for (int i = j - 1; i >= 0; i--)
+        {
+            d[i] = (int)(sum % 10);
+            sum = sum / 10;
+        }
+
+        return d;
+    }
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+
+    // --------------- O(n) 308ms --------------- 37.9MB --------------- (72% 18%) 
+    public IList<int> AddToArrayForm_3(int[] A, int K)
+    {
+        List<int> d = new List<int>();
+
+        for (int i = A.Length - 1; i >= 0; i--)
+        {
+            int x = A[i] + K;
+            d.Add(x % 10);
+            K = x / 10;
+        }
+
+        while (K > 0)
+        {
+            d.Add(K % 10);
+            K = K / 10;
+        }
+
+        d.Reverse();
+        return d;
+    }
+
+    // --------------- O(n) 312ms --------------- 37.7MB --------------- (63% 70%) 
+    public IList<int> AddToArrayForm_4(int[] A, int K)
+    {
+        List<int> d = new List<int>();
+
+        int i = A.Length - 1;
+        while (i >= 0 || K > 0)
+        {
+            K = K + (i >= 0 ? A[i--] : 0);
+            d.Add(K % 10);
+            K = K / 10;
+        }
+
+        d.Reverse();
+        return d;
+    }
+
+    // --------------- O(n) 296ms --------------- 36.7MB --------------- (95% 88%) ※ 
+    public IList<int> AddToArrayForm_5(int[] A, int K)
+    {
+        List<int> d = new List<int>(A);
+
+        for (int i = A.Length - 1; i >= 0; i--)
+        {
+            int x = A[i] + K;
+            d[i] = x % 10;
+            K = x / 10;
+        }
+
+        while (K > 0)
+        {
+            d.Insert(0, K % 10);
+            K = K / 10;
+        }
+
+        return d;
+    }
+}
+/**************************************************************************************************************
+ * AddToArrayForm_3/5 AddToArrayForm_4                                                                        *
+ **************************************************************************************************************/
---
+++ CSharp/Available Captures for Rook.cs
@@ -0,0 +1,157 @@
+﻿//Source  : https://leetcode.com/problems/available-captures-for-rook/
+//Author  : Xinruo Shi
+//Date    : 2019-07-28
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * On an 8 x 8 chessboard, there is one white rook.  There also may be empty squares, white bishops, and black pawns. 
+ * These are given as characters 'R', '.', 'B', and 'p' respectively. Uppercase characters represent white pieces, and lowercase characters represent black pieces.
+ * The rook moves as in the rules of Chess: it chooses one of four cardinal directions (north, east, west, and south), 
+ * then moves in that direction until it chooses to stop, reaches the edge of the board, or captures an opposite colored pawn by moving to the same square it occupies.  
+ * Also, rooks cannot move into the same square as other friendly bishops.
+ * Return the number of pawns the rook can capture in one move.
+ * 
+ * Note:
+ * board.length == board[i].length == 8
+ * board[i][j] is either 'R', '.', 'B', or 'p'
+ * There is exactly one cell with board[i][j] == 'R'
+ * 
+ * Input: [[".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],
+ *         [".",".",".","R",".",".",".","p"],[".",".",".",".",".",".",".","."],
+ *         [".",".",".",".",".",".",".","."],[".",".",".","p",".",".",".","."],
+ *         [".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".","."]]
+ * Output: 3
+ * Explanation: 
+ * In this example the rook is able to capture all the pawns.
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution999
+{
+    // --------------- O(n) 100ms --------------- 21.9MB --------------- (26% 6%) 
+    public int NumRookCaptures_1(char[][] board)
+    {
+        int x = -1;
+        int y = -1;
+
+        for (int i = 0; i < board.Length; i++)
+        {
+            for (int j = 0; j < board[0].Length; j++)
+            {
+                if (board[i][j] == 'R')
+                {
+                    x = i;
+                    y = j;
+                    break;
+                }
+            }
+        }
+
+        int count = 0;
+        // west
+        for (int i = x - 1; i >= 0; i--)
+        {
+            if (board[i][y] == '.')
+            {
+                continue;
+            }
+            else if (board[i][y] == 'B')
+            {
+                break;
+            }
+            else if (board[i][y] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        // east
+        for (int i = x + 1; i < board[0].Length; i++)
+        {
+            if (board[i][y] == '.')
+            {
+                continue;
+            }
+            else if (board[i][y] == 'B')
+            {
+                break;
+            }
+            else if (board[i][y] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        // south
+        for (int i = y + 1; i < board[0].Length; i++)
+        {
+            if (board[x][i] == '.')
+            {
+                continue;
+            }
+            else if (board[x][i] == 'B')
+            {
+                break;
+            }
+            else if (board[x][i] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        // north
+        for (int i = y - 1; i >= 0; i--)
+        {
+            if (board[x][i] == '.')
+            {
+                continue;
+            }
+            else if (board[x][i] == 'B')
+            {
+                break;
+            }
+            else if (board[x][i] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+
+        return count;
+    }
+
+    // --------------- O(n) 96ms --------------- 21.9MB --------------- (49% 6%) ※
+    public int NumRookCaptures_2(char[][] board)
+    {
+        int x = -1;
+        int y = -1;
+
+        for (int i = 0; i < board.Length; i++)
+        {
+            for (int j = 0; j < board[0].Length; j++)
+            {
+                if (board[i][j] == 'R')
+                {
+                    x = i;
+                    y = j;
+                    break;
+                }
+            }
+        }
+
+        int count = 0;
+
+        int[,] d = new int[,] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        for (int i = 0; i < 4; i++)
+        {
+            for (int x0 = x + d[i, 0], y0 = y + d[i, 1]; x0 < board[0].Length && y0 < board[0].Length && x0 >= 0 && y0 >= 0; x0 += d[i, 0], y0 += d[i, 1])
+            {
+                if (board[x0][y0] == 'p') count++;
+                if (board[x0][y0] != '.') break;
+            }
+        }
+
+        return count;
+    }
+}
