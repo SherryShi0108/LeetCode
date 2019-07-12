90d8a886b6cc789b7ae89475a91ccb392656ccc7
XinRuoShi <XinruoShi>
2019-07-12 16:50:52 +0800

    448 724

---
+++ CSharp/Find All Numbers Disappeared in an Array.cs
@@ -0,0 +1,146 @@
+﻿//Source  : https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+//Author  : Xinruo Shi
+//Date    : 2019-06-12
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+ * Find all the elements of [1, n] inclusive that do not appear in this array.
+ * Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+ * 
+ * Input: nums = [4,3,2,7,8,2,3,1]
+ * Output: [5,6]
+ * 
+ *******************************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+public class Solution448
+{
+    // --------------- O(n) 352ms --------------- 45.1MB --------------- (29% 30%)
+    /*
+     * But O(space)!=O(1),using extra dictionary  
+     */
+    public IList<int> FindDisappearedNumbers_1(int[] nums)
+    {
+        List<int> L = new List<int>();
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        for (int i = 1; i < nums.Length + 1; i++)
+        {
+            d.Add(i, 1);
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (d.ContainsKey(nums[i]))
+            {
+                d.Remove(nums[i]);
+            }
+        }
+        foreach (var item in d)
+        {
+            L.Add(item.Key);
+        }
+
+        return L;
+    }
+
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+    /*
+     * Time Limit Exceeded
+     */
+    public IList<int> FindDisappearedNumbers_2(int[] nums)
+    {
+        List<int> L = new List<int>();
+        for (int i = 1; i < nums.Length + 1; i++)
+        {
+            L.Add(i);
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (L.Contains(nums[i]))
+            {
+                L.Remove(nums[i]);
+            }
+        }
+
+        return L;
+    }
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+
+    // --------------- O(n) 336ms --------------- 43.1MB --------------- (52% 40%)
+    /*
+     * But O(space)!=O(1),using extra int[]  
+     */
+    public IList<int> FindDisappearedNumbers_3(int[] nums)
+    {
+        List<int> L = new List<int>();
+        int[] temp = new int[nums.Length+1];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            temp[nums[i]]++;
+        }
+        for (int i = 1; i < temp.Length; i++)
+        {
+            if (temp[i] == 0)
+            {
+                L.Add(i);
+            }
+        }
+        return L;
+    }
+
+    // --------------- O(n) 336ms --------------- 42.5MB --------------- (52% 53%) ※ 
+    public IList<int> FindDisappearedNumbers_4(int[] nums)
+    {
+        List<int> L = new List<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int x = (nums[i] > 0 ? nums[i] : -nums[i]) - 1;
+            nums[x] = nums[x] > 0 ? -nums[x] : nums[x];
+        }
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                L.Add(i + 1);
+            }
+        }
+        return L;
+    }
+
+    /*
+     * Time Limit Exceeded
+     *  O(time)!=O(n)
+     */
+    public IList<int> FindDisappearedNumbers_5(int[] nums)
+    {
+        List<int> L = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            while (nums[i] != i + 1 && nums[i] != nums[nums[i] - 1])
+            {
+                int temp = nums[i];
+                nums[i] = nums[nums[i] - 1];
+                nums[nums[i] - 1] = temp;
+            }
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != i + 1)
+            {
+                L.Add(i+1);
+            }
+        }
+       
+        return L;
+    }
+}
+
+/**************************************************************************************************************
+ * FindDisappearedNumbers_4                                                                                   *
+ **************************************************************************************************************/
---
+++ CSharp/Find Pivot Index.cs
@@ -0,0 +1,147 @@
+﻿//Source  : https://leetcode.com/problems/find-pivot-index/
+//Author  : Xinruo Shi
+//Date    : 2019-06-28
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array of integers nums, write a method that returns the "pivot" index of this array.
+ * We define the pivot index as the index where the sum of the numbers to the left of the index is equal to the sum of the numbers to the right of the index.
+ * If no such index exists, we should return -1. If there are multiple pivot indexes, you should return the left-most pivot index.
+ * 
+ * Note:
+ * The length of nums will be in the range [0, 10000].
+ * Each element nums[i] will be an integer in the range [-1000, 1000].
+ * 
+ * Input: nums = [1, 7, 3, 6, 5, 6]
+ * Output: 3
+ * Explanation: The sum of the numbers to the left of index 3 (nums[3] = 6) is equal to the sum of numbers to the right of index 3.Also, 3 is the first index where this occurs.
+ * 
+ * Input: nums = [1, 2, 3]
+ * Output: -1
+ * Explanation: There is no index that satisfies the conditions in the problem statement.
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution724
+{
+    // --------------- O(n^2) 1116ms --------------- 28.8MB --------------- (9% 48%)
+    public int PivotIndex_1(int[] nums)
+    {
+        int L = 0;
+        int R = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                L += nums[j];
+            }
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                R += nums[j];
+            }
+            if (L == R)
+            {
+                return i;
+            }
+
+            L = 0; R = 0;
+        }
+        return -1;
+    }
+
+    // --------------- O(n) 116ms --------------- 29MB --------------- (90% 15%) ※
+    public int PivotIndex_2(int[] nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (leftSum == sum - leftSum - nums[i])
+            {
+                return i;
+            }
+            leftSum += nums[i];
+
+        }
+        return -1;
+    }
+
+    // --------------- O(n) 120ms --------------- 28.9MB --------------- (68% 17%)
+    public int PivotIndex_3(int[] nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+
+        int sumL = 0;
+        int sumR = sum;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sumL = sum - sumR;
+            sumR = sumR - nums[i];
+
+            if (sumL == sumR)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // --------------- O(n) 148ms --------------- 29MB --------------- (21% 15%) 
+    public int PivotIndex_4(int[] nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (leftSum * 2 + nums[i] == sum)
+            {
+                return i;
+            }
+            leftSum += nums[i];          
+        }
+
+        return -1;
+    }
+
+    // --------------- O(n) 120ms --------------- 28.9MB --------------- (38% 20%) 
+    public int PivotIndex_5(int[] nums)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if ((sum = (sum - nums[i])) == leftSum)
+                return i;
+            else
+                leftSum += nums[i];
+        }
+
+        return -1;
+    }
+
+
+
+}
+/**************************************************************************************************************
+ * PivotIndex_2 PivotIndex_3/4/5                                                                              *
+ **************************************************************************************************************/
