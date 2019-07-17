7fe5b36b1c0a59cf9380e7e922aa12770dd26e2d
XinRuoShi <XinruoShi>
2019-07-17 12:10:12 +0800

    532 832

---
+++ CSharp/Flipping an Image.cs
@@ -0,0 +1,88 @@
+﻿//Source  : https://leetcode.com/problems/flipping-an-image/
+//Author  : Xinruo Shi
+//Date    : 2019-07-03
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+ * To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+ * To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+ * 
+ * Note:
+ * 1 <= A.length = A[0].length <= 20
+ * 0 <= A[i][j] <= 1
+ * 
+ * Input: [[1,1,0],[1,0,1],[0,0,0]]
+ * Output: [[1,0,0],[0,1,0],[1,1,1]]
+ * Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+ * 
+ * Input: [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+ * Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ * Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+ * 
+ *******************************************************************************************************************************/
+
+public class Solution832
+{
+    // --------------- O(n)=O(A.Length) 268ms --------------- 30.1MB --------------- (49% 90%) 
+    public int[][] FlipAndInvertImage_1(int[][] A)
+    {
+        foreach (int[] item in A)
+        {
+            for (int i = 0; i < A[0].Length / 2; i++)
+            {
+                int temp = item[i];
+                item[i] = item[A[0].Length - i - 1];
+                item[A[0].Length - i - 1] = temp;
+            }
+            for (int i = 0; i < A[0].Length; i++)
+            {
+                if (item[i] == 0)
+                {
+                    item[i] = 1;
+                }
+                else
+                {
+                    item[i] = 0;
+                }
+            }
+        }
+        return A;
+    }
+
+    // --------------- O(n)=O(A.Length) 292ms --------------- 30.1MB --------------- (8% 90%) 
+    public int[][] FlipAndInvertImage_2(int[][] A)
+    {
+        foreach (int[] item in A)
+        {
+            for (int i = 0; i < (A[0].Length + 1) / 2; i++)
+            {
+                int temp = item[i] ^ 1;
+                item[i] = item[A[0].Length - i - 1] ^ 1;
+                item[A[0].Length - i - 1] = temp;
+            }
+        }
+        return A;
+    }
+
+    // --------------- O(n)=O(A.Length) 284ms --------------- 30MB --------------- (17% 97%) ※
+    public int[][] FlipAndInvertImage_3(int[][] A)
+    {
+        foreach (int[] item in A)
+        {
+            for (int i = 0; i * 2 < A[0].Length; i++)
+            {
+                if (item[i] == item[A[0].Length - 1 - i])
+                {
+                    int x = item[i] ^ 1;     //或者 x=1-item[i];
+                    item[i] = item[A[0].Length - 1 - i] = x;
+                }
+            }
+        }
+        return A;
+    }
+}
+/**************************************************************************************************************
+ * FlipAndInvertImage_1/2/3                                                                                   *
+ **************************************************************************************************************/
---
+++ CSharp/K-diff Pairs in an Array.cs
@@ -0,0 +1,67 @@
+﻿//Source  : https://leetcode.com/problems/k-diff-pairs-in-an-array/
+//Author  : Xinruo Shi
+//Date    : 2019-06-15
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array. 
+ * Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their absolute difference is k.
+ * 
+ * Note:
+ * The pairs (i, j) and (j, i) count as the same pair.
+ * The length of the array won't exceed 10,000.
+ * All the integers in the given input belong to the range: [-1e7, 1e7].
+ * 
+ * Input: nums = [3, 1, 4, 1, 5], k = 2
+ * Output: 2
+ * Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+ * Although we have two 1s in the input, we should only return the number of unique pairs.
+ * 
+ * Input: nums = [1, 3, 1, 5, 4], k = 0
+ * Output: 1
+ * Explanation: There is one 0-diff pair in the array, (1, 1).
+ * 
+ *******************************************************************************************************************************/
+
+using System.Collections.Generic;
+
+public class Solution532
+{
+    // --------------- O(n) 120ms --------------- 31.9MB --------------- (66% 8%) ※
+    /*
+     * Why k>=-1? cause absolute difference!!! so k>=0!!!
+     */
+    public int FindPairs_1(int[] nums, int k)
+    {
+        Dictionary<int, int> d = new Dictionary<int, int>();
+
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (d.ContainsKey(nums[i]))
+            {
+                d[nums[i]]++;
+            }
+            else
+            {
+                d[nums[i]]=1;
+            }
+        }
+        foreach (var item in d)
+        {
+            if (k == 0 && item.Value > 1)
+            {
+                count++;
+            }
+            if (k >= 1 && d.ContainsKey(item.Key+k))
+            {
+                count++;
+            }
+        } 
+        return count;
+    }
+}
+/**************************************************************************************************************
+ * FindPairs_1                                                                                                *
+ **************************************************************************************************************/
