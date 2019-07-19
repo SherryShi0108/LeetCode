3435659bb31984282777ab41061828d6874b6aac
XinRuoShi <XinruoShi>
2019-07-19 14:24:15 +0800

    840 849

---
+++ CSharp/Magic Squares In Grid.cs
@@ -0,0 +1,89 @@
+﻿//Source  : https://leetcode.com/problems/magic-squares-in-grid/
+//Author  : Xinruo Shi
+//Date    : 2019-07-04
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * A 3 x 3 magic square is a 3 x 3 grid filled with distinct numbers from 1 to 9 such that each row, column, and both diagonals all have the same sum.
+ * Given an grid of integers, how many 3 x 3 "magic square" subgrids are there?  (Each subgrid is contiguous).
+ * 
+ * Note:
+ * 1 <= grid.length <= 10
+ * 1 <= grid[0].length <= 10
+ * 0 <= grid[i][j] <= 15
+ * 
+ * Input: [[4,3,8,4],
+ *         [9,5,1,9],
+ *         [2,7,6,2]]
+ * Output: 1
+ * Explanation: 
+ *      The following subgrid is a 3 x 3 magic square:
+ *             438
+ *             951
+ *             276
+ *      while this one is not:
+ *             384
+ *             519
+ *             762
+ *      In total, there is only one magic square inside the given grid.
+ * 
+ *******************************************************************************************************************************/
+
+using System.Collections.Generic;
+
+public class Solution840
+{
+    // --------------- O(n)=O(grid.length) 88ms --------------- 22.4MB --------------- (100% 20%) 
+    /*
+     * Boring problem
+     */
+    public int NumMagicSquaresInside_1(int[][] grid)
+    {
+        int N1 = grid.Length;
+        int N2 = grid[0].Length;
+        int count = 0;
+
+        for (int i = 0; i < N1 - 2; i++)
+        {
+            for (int j = 0; j < N2 - 2; j++)
+            {
+                if (grid[i][j] + grid[i][j + 1] + grid[i][j + 2] != 15) continue;
+                if (grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2] != 15) continue;
+                if (grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2] != 15) continue;
+                if (grid[i][j] + grid[i + 1][j] + grid[i + 2][j] != 15) continue;
+                if (grid[i][j + 1] + grid[i + 1][j + 1] + grid[i + 2][j + 1] != 15) continue;
+                if (grid[i][j + 2] + grid[i + 1][j + 2] + grid[i + 2][j + 2] != 15) continue;
+                if (grid[i][j] + grid[i + 1][j + 1] + grid[i + 2][j + 2] != 15) continue;
+                if (grid[i + 2][j] + grid[i + 1][j + 1] + grid[i][j + 2] != 15) continue;
+
+                List<int> L = new List<int>();
+                L.Add(grid[i][j]);L.Add(grid[i][j + 1]);L.Add(grid[i][j + 2]);
+                L.Add(grid[i + 1][j]);L.Add(grid[i + 1][j + 1]);L.Add(grid[i + 1][j + 2]);
+                L.Add(grid[i + 2][j]);L.Add(grid[i + 2][j + 1]);L.Add(grid[i + 2][j + 2]);
+                if (IsAll9(L))
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public bool IsAll9(List<int> a)
+    {
+        if (!a.Contains(1)) return false;
+        if (!a.Contains(2)) return false;
+        if (!a.Contains(3)) return false;
+        if (!a.Contains(4)) return false;
+        if (!a.Contains(5)) return false;
+        if (!a.Contains(6)) return false;
+        if (!a.Contains(7)) return false;
+        if (!a.Contains(8)) return false;
+        if (!a.Contains(9)) return false;
+
+        return true;
+    }
+}
+/**************************************************************************************************************
+ * NumMagicSquaresInside_1                                                                                    *
+ **************************************************************************************************************/
---
+++ CSharp/Maximize Distance to Closest Person.cs
@@ -0,0 +1,173 @@
+﻿//Source  : https://leetcode.com/problems/maximize-distance-to-closest-person/
+//Author  : Xinruo Shi
+//Date    : 2019-07-05
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * In a row of seats, 1 represents a person sitting in that seat, and 0 represents that the seat is empty. 
+ * There is at least one empty seat, and at least one person sitting.
+ * Alex wants to sit in the seat such that the distance between him and the closest person to him is maximized. 
+ * Return that maximum distance to closest person.
+ * 
+ * Note:
+ * 1 <= seats.length <= 20000
+ * seats contains only 0s or 1s, at least one 0, and at least one 1.
+ * 
+ * Input: [1,0,0,0,1,0,1]
+ * Output: 2
+ * Explanation: If Alex sits in the second open seat (seats[2]), then the closest person has distance 2.
+ *              If Alex sits in any other open seat, the closest person has distance 1.
+ *              Thus, the maximum distance to the closest person is 2.
+ * 
+ * Input: [1,0,0,0]
+ * Output: 3
+ * Explanation: If Alex sits in the last seat, the closest person is 3 seats away.This is the maximum distance possible, so the answer is 3.
+ * 
+ *******************************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+public class Solution849
+{
+    // --------------- O(n) 112ms --------------- O(n) 26.4MB --------------- (55% 11%) 
+    /*
+     * new int[] to store the index of 1
+     */
+    public int MaxDistToClosest_1(int[] seats)
+    {
+        int[] temp = new int[seats.Length];
+        for (int i = 0; i < temp.Length; i++)
+        {
+            temp[i] = -1;
+        }
+
+        int j = 0;
+        for (int i = 0; i < seats.Length; i++)
+        {
+            if (seats[i] == 1)
+            {
+                temp[j] = i;
+                j++;
+            }
+        }
+
+        int count = 0;
+        int max = 0;
+        for (int i = 0; i < temp.Length - 1; i++)
+        {
+            if (temp[0] != 0)
+            {
+                count = temp[0];
+                max = max > count ? max : count;
+            }
+            if (temp[i + 1] == -1)
+            {
+                count = seats.Length - 1 - temp[i];
+                max = max > count ? max : count;
+                break;
+            }
+            else
+            {
+                count = (temp[i + 1] - temp[i]) / 2;
+                max = max > count ? max : count;
+            }
+        }
+        return max;
+    }
+
+    // --------------- O(n) 108ms --------------- O(n) 25.9MB --------------- (73% 44%) ※  
+    /*
+     * easy-understanding
+     */
+    public int MaxDistToClosest_2(int[] seats)
+    {
+        int count = 0;
+        int max = 0;
+
+        for (int i = 0; i < seats.Length; i++)
+        {
+            if (seats[i] == 0)
+            {
+                count++;
+                max = max > (count + 1) / 2 ? max : (count + 1) / 2;
+            }
+            else
+            {
+                count = 0;
+            }
+        }
+        for (int i = 0; i < seats.Length; i++)
+        {
+            if (seats[i] == 1)
+            {
+                max = max > i ? max : i;
+                break;
+            }
+        }
+        for (int i = seats.Length - 1; i >= 0; i--)
+        {
+            if (seats[i] == 1)
+            {
+                max = max > seats.Length - 1 - i ? max : seats.Length - 1 - i;
+                break;
+            }
+        }
+        return max;
+    }
+
+    // --------------- O(n) 128ms --------------- O(n) 25.9MB --------------- (11% 47%) 
+    public int MaxDistToClosest_3(int[] seats)
+    {
+        int max = 0;
+        int countMedium = 0;
+
+        while (seats[max] == 0)
+        {
+            max++;
+        }
+
+        for (int i = max + 1; i < seats.Length; i++)
+        {
+            if (seats[i] == 0)
+            {
+                countMedium++;
+            }
+            else
+            {
+                max = max > (countMedium + 1) / 2 ? max : (countMedium + 1) / 2;
+                countMedium = 0;
+            }
+        }
+        max = max > countMedium ? max : countMedium;
+        return max;
+    }
+
+    // --------------- O(n) 128ms --------------- O(n) 25.8MB --------------- (11% 58%) 
+    public int MaxDistToClosest_4(int[] seats)
+    {
+        int left = -1;
+        int max = 0;
+
+        for (int i = 0; i < seats.Length; i++)
+        {
+            if (seats[i] == 0) continue;
+            if (left == -1)
+            {
+                max = max > i ? max : i;
+            }
+            else
+            {
+                max = max > (i - left) / 2 ? max : (i - left) / 2;
+            }
+            left = i;
+        }
+
+        max = max > (seats.Length - 1 - left) ? max : (seats.Length - 1 - left);
+        return max;
+    }    
+}
+/**************************************************************************************************************
+ * MaxDistToClosest_2                                                                                         *
+ **************************************************************************************************************/
