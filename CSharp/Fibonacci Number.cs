0dd72581a42c9aeefc8e2e1688e5e6a476b12781
SherryShi0108 <sxr1234567890@outlook.com>
2019-07-22 15:06:31 +0800

    Update Fibonacci Number.cs

--- CSharp/Fibonacci Number.cs
+++ CSharp/Fibonacci Number.cs
@@ -64,6 +64,19 @@ public class Solution509
     {
         return Fibs[N];
     }
+    
+    // Recursive
+    public int Fib(int n)
+    {
+        if (n < 2)
+        {
+            return n;
+        }
+        else
+        {
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
 }
 /**************************************************************************************************************
  * Fib_1 Fib_2                                                                                                *
