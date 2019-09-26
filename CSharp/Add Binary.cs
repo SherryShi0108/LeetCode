ec8495bcfca2dce820f5c271cb9b2067e383d5fd
XinRuoShi <XinRuoShi>
2019-09-26 15:14:43 +0800

    67 908

---
+++ CSharp/Add Binary.cs
@@ -0,0 +1,162 @@
+﻿//Source  : https://leetcode.com/problems/add-binary/
+//Author  : Xinruo Shi
+//Date    : 2019-09-02
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given two binary strings, return their sum (also a binary string).
+ * The input strings are both non-empty and contains only characters 1 or 0.
+ * 
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ * 
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ * 
+ *******************************************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+public class Solution67
+{
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+    /*
+     * stack overflow , it's all "add"
+     */
+    public string AddBinary_1(string a, string b)
+    {
+        int x = ChangeStringToInt(a);
+        int y = ChangeStringToInt(b);
+        int sum = x + y;
+        if (sum == 0)
+        {
+            return "0";
+
+        }
+
+        List<int> L = new List<int>();
+        while (sum > 0)
+        {
+            L.Add(sum % 2);
+            sum /= 2;
+        }
+
+        string result = string.Empty;
+
+        foreach (int t in L)
+        {
+            result = t + result;
+        }
+
+        return result;
+    }
+
+    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++
+
+    public int ChangeStringToInt(string s)
+    {
+        int result = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            result = result * 2 + int.Parse(s[i].ToString());
+        }
+
+        return result;
+    }
+
+    // --------------- O(n) 96ms --------------- 25.3MB --------------- (45% 8%)
+    /*
+     * "0" "0" should be "0" not "00"
+     */
+    public string AddBinary_2(string a, string b)
+    {
+        List<int> L1 = new List<int>();
+        for (int i = a.Length - 1; i >= 0; i--)
+        {
+            L1.Add(int.Parse(a[i].ToString()));
+        }
+
+        List<int> L2 = new List<int>();
+        for (int i = b.Length - 1; i >= 0; i--)
+        {
+            L2.Add(int.Parse(b[i].ToString()));
+        }
+
+        List<int> L3 = new List<int>();
+
+        int k = 0;
+        for (int i = 0; i < L1.Count || i < L2.Count || k == 1; i++)
+        {
+            int x = i < L1.Count ? L1[i] : 0;
+            int y = i < L2.Count ? L2[i] : 0;
+
+            int t = x + y + k;
+            L3.Add(t % 2);
+            k = t / 2;
+        }
+
+        string result = "";
+        foreach (int t in L3)
+        {
+            result = t + result;
+        }
+
+        return result;
+    }
+
+    // --------------- O(n) 88ms --------------- 24.4MB --------------- (90% 8%) ※
+    public string AddBinary_3(string a, string b)
+    {
+        string result = "";
+        int k = 0;
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+
+        while (i >= 0 || j >= 0 || k == 1)
+        {
+            k = k + (i >= 0 ? a[i--] - '0' : 0);
+            k = k + (j >= 0 ? b[j--] - '0' : 0);
+
+            result = (k % 2).ToString() + result;
+            k /= 2;
+        }
+
+        return result;
+    }
+
+    // --------------- O(n) 88ms --------------- 25.2MB --------------- (90% 8%) 
+    /*
+     * similar to 3
+     */
+    public string AddBinary_4(string a, string b)
+    {
+        string result = "";
+        int k = 0;
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+
+        while (i>=0||j>=0||k==1)
+        {
+            int sum = k;
+            if (i >= 0)
+            {
+                sum += (a[i--] - '0');
+            }
+
+            if (j >= 0)
+            {
+                sum += (b[j--] - '0');
+            }
+
+            result = sum % 2 + result;
+            k = sum/2;
+        }
+
+        return result;
+    }
+}
+/**************************************************************************************************************
+ * AddBinary_3 / AddBinary_4                                                                                  *
+ **************************************************************************************************************/
---
+++ CSharp/Smallest Range I.cs
@@ -0,0 +1,69 @@
+﻿//Source  : https://leetcode.com/problems/smallest-range-i/
+//Author  : Xinruo Shi
+//Date    : 2019-09-20
+//Language: C#
+
+/*******************************************************************************************************************************
+ * 
+ * Given an array A of integers, for each integer A[i] we may choose any x with -K <= x <= K, and add x to A[i].
+ * After this process, we have some array B.
+ * Return the smallest possible difference between the maximum value of B and the minimum value of B.
+ * 
+ * Input: A = [1], K = 0
+ * Output: 0
+ * Explanation: B = [1]
+ * 
+ * Input: A = [0,10], K = 2
+ * Output: 6
+ * Explanation: B = [2,8]
+ * 
+ * Input: A = [1,3,6], K = 3
+ * Output: 0
+ * Explanation: B = [3,3,3] or B = [4,4,4]
+ * 
+ * Note: 1 <= A.length <= 10000
+ *       0 <= A[i] <= 10000
+ *       0 <= K <= 10000
+ * 
+ *******************************************************************************************************************************/
+
+using System.Linq;
+using System.Runtime.InteropServices;
+
+public class Solution908
+{
+    // --------------- O(n) 124ms --------------- 27.6MB --------------- (24% 100%)
+    /*
+     * min(A)+k,max(A)-k,and judge if these overlapping
+     */
+    public int SmallestRangeI_1(int[] A, int K)
+    {
+        int x = A.Max() - K;
+        int y = A.Min() + K;
+
+        if (x <= y)
+        {
+            return 0;
+        }
+        
+        return x-y;
+    }
+
+    // --------------- O(n) 104ms --------------- 27.3MB --------------- (99% 100%) ※
+    public int SmallestRangeI_2(int[] A, int K)
+    {
+        int max = A[0];
+        int min = A[0];
+        foreach (int t in A)
+        {
+            max = max > t ? max : t;
+            min = min < t ? min : t;
+        }
+
+        int result = (min + K) - (max - K) > 0 ? 0 : (max - K) - (min + K);
+        return result;
+    }
+}
+/**************************************************************************************************************
+ * SmallestRangeI_2                                                                                           *
+ **************************************************************************************************************/
