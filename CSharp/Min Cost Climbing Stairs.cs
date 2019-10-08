78ca40580e5216accec230abb34681fe14eaf4f4
SherryShi0108 <sxr1234567890@outlook.com>
2019-10-08 11:27:24 +0800

    Update Min Cost Climbing Stairs.cs

--- CSharp/Min Cost Climbing Stairs.cs
+++ CSharp/Min Cost Climbing Stairs.cs
@@ -20,7 +20,7 @@
  * Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
  * Output: 6
  * Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
- * ※ 
+ * 
  *******************************************************************************************************************************/
 
 //动态规划
@@ -39,6 +39,17 @@ public class Solution746
         }
 
         return C[C.Length-1];
+    }
+    
+    // --------------- O(n) 96ms --------------- 24.3MB --------------- (83% 13%)  ※
+    public int MinCostClimbingStairs_2(int[] cost)
+    {
+        for (int i = 2; i < cost.Length; i++)
+        {
+            cost[i] += (cost[i - 2] < cost[i - 1] ? cost[i - 2] : cost[i - 1]);
+        }
+
+        return cost[cost.Length - 1] < cost[cost.Length - 2] ? cost[cost.Length - 1] : cost[cost.Length - 2];
     }
 }
 /**************************************************************************************************************
