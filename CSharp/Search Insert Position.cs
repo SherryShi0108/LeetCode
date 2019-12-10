f25264f080f2352b373e5f0dc2cfff2ee0769a1f
SherryShi0108 <sxr1234567890@outlook.com>
2019-12-10 14:49:55 +0800

    Update Search Insert Position.cs

--- CSharp/Search Insert Position.cs
+++ CSharp/Search Insert Position.cs
@@ -127,6 +127,32 @@ public class Solution
 
         return left ;
     }
+    
+    // --------------- O(n) 96ms --------------- 23.9MB --------------- (63% 5%)
+    /* Normal Solution */
+    public int SearchInsert_7(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length-1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid-1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
 }
 /**************************************************************************************************************
  * SearchInsert_6                                                                                             *
