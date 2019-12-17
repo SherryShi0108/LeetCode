476a3392131c0a0a47c335203119e2db7a32e388
SherryShi0108 <sxr1234567890@outlook.com>
2019-12-17 09:41:49 +0800

    Update Peak Index in a Mountain Array.cs

--- CSharp/Peak Index in a Mountain Array.cs
+++ CSharp/Peak Index in a Mountain Array.cs
@@ -55,7 +55,7 @@ public class Solution852
         return i;
     }
 
-    // --------------- O(logn) 104ms --------------- O(1) 25.9MB --------------- (49% 100%) ※
+    // --------------- O(logn) 104ms --------------- O(1) 25.9MB --------------- (49% 100%) 
     /*
      * use binary search , faster than 1/2 because O(time) = O(logn)
      */
@@ -78,12 +78,33 @@ public class Solution852
 
         return left;
     }
+    
+    // --------------- O(logn) 104ms --------------- O(1) 26MB --------------- (49% 100%) ※
+    public int PeakIndexInMountainArray_4(int[] A)
+    {
+        int i = 0;
+        int j = A.Length;
+        while (i<j)
+        {
+            int mid = i + (j - i) / 2;
+            if (mid== A.Length-1 || A[mid] < A[mid+1])
+            {
+                i = mid+1;
+            }
+            else
+            {
+                j = mid;
+            }
+        }
+
+        return i;
+    }
 
     // --------------- O(n) 96ms --------------- O(1) 25.7MB --------------- (90% 100%)
     /*
      * find the max's index
      */
-    public int PeakIndexInMountainArray_4(int[] A)
+    public int PeakIndexInMountainArray_5(int[] A)
     {
         int index = 0;
         int max = 0;
