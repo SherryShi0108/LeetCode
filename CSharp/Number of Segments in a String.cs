1373d45ff1751948be849c1f9845519253c778b7
SherryShi0108 <sxr1234567890@outlook.com>
2020-04-07 14:29:06 +0800

    Update Number of Segments in a String.cs

--- CSharp/Number of Segments in a String.cs
+++ CSharp/Number of Segments in a String.cs
@@ -17,7 +17,7 @@ using System.Data;
 
 public class Solution434
 {
-    // --------------- O(n) 68ms --------------- 21.3MB --------------- (96% 50%) ※
+    // --------------- O(n) 68ms --------------- 21.3MB --------------- (96% 50%) 
     /*
      * it's important to use (i==0 || s[i-1]==' ')
      */
@@ -34,7 +34,27 @@ public class Solution434
 
         return count;
     }
+    
+     // --------------- O(n) 68ms --------------- 21.3MB --------------- (97% 50%) ※
+    /*
+     * similar to 1 , more easy-understanding
+     */
+    public int CountSegments_2(string s)
+    {
+        if (s == null || s.Length < 1 ) return 0;
+
+        int count = 0;
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i - 1] == ' ' && s[i] != ' ')
+            {
+                count++;
+            }
+        }
+
+        return s[0] == ' ' ? count : count + 1;
+    }
 }
 /**************************************************************************************************************
- * CountSegments_1                                                                                            *
+ * CountSegments_2                                                                                            *
  **************************************************************************************************************/
