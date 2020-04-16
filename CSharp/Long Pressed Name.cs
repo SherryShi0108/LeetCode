6d19a2ca896b29f8b272c8c242400ff33a81d8da
SherryShi0108 <sxr1234567890@outlook.com>
2020-04-16 10:40:50 +0800

    Update Long Pressed Name.cs

--- CSharp/Long Pressed Name.cs
+++ CSharp/Long Pressed Name.cs
@@ -97,7 +97,7 @@ public class Solution925
         return true;
     }
 
-    // --------------- O(n) 72ms --------------- 22.7MB --------------- (82% 100%) ※
+    // --------------- O(n) 72ms --------------- 22.7MB --------------- (82% 100%) 
     /*
      * Amazing solution !!! donnot understand , remember it !!!
      */
@@ -113,6 +113,35 @@ public class Solution925
             else if(j==0||typed[j]!=typed[j-1])
             {
                 return false;
+            }
+        }
+
+        return i == name.Length;
+    }
+    
+     // --------------- O(n) 72ms --------------- 22MB --------------- (80% 100%) ※
+    /*
+     * improve 2 : easy-unstanding
+     */
+    public bool IsLongPressedName_2_2(string name, string typed)
+    {
+        int i = 0;
+        int j = 0;
+
+        while (j < typed.Length)
+        {
+            if (i < name.Length && name[i] == typed[j])
+            {
+                i++;
+                j++;
+            }
+            else if (j == 0 || typed[j] != typed[j - 1])
+            {
+                return false;
+            }
+            else
+            {
+                j++;
             }
         }
 
