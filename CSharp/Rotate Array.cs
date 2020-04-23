eee4469b86b9f2aa814f92eaaa0a2f8ff3a05283
SherryShi0108 <sxr1234567890@outlook.com>
2020-04-23 12:28:12 +0800

    Update Rotate Array.cs

--- CSharp/Rotate Array.cs
+++ CSharp/Rotate Array.cs
@@ -110,7 +110,7 @@ public class Solution189
     }
     
     // --------------- O(n) 240ms --------------- O(1)32MB --------------- (86% 9%) ※
-    public void Rotate_3(int[] nums, int k)
+    public void Rotate_3_1(int[] nums, int k)
     {
         int count = 0;
         int i = -1;
@@ -129,6 +129,29 @@ public class Solution189
 
                 if (++count == nums.Length) return;
             } while (i != currentIndex);
+        }
+    }
+    
+    // --------------- O(n) 228ms --------------- O(1)32MB --------------- (99.8% 9%) ※
+    public void Rotate_3_2(int[] nums, int k)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int currentIndex = i;
+            int currentValue = nums[currentIndex];
+            while (true)
+            {
+                int nextIndex = (k + currentIndex) % nums.Length;
+                int nextValue = nums[nextIndex];
+
+                nums[nextIndex] = currentValue;
+                currentIndex = nextIndex;
+                currentValue = nextValue;
+
+                if (++count == nums.Length) return;
+                if (currentIndex == i) break;
+            }
         }
     }
 }
