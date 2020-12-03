1f4d1e47a46a20e2e2ad9044dacf1edeb1dcdd17
SherryShi0108 <sxr1234567890@outlook.com>
2020-12-03 10:22:29 +0800

    Update Non-decreasing Array.cs

--- CSharp/Non-decreasing Array.cs
+++ CSharp/Non-decreasing Array.cs
@@ -68,7 +68,39 @@ public class Solution665
 
         return true;
     }
+    
+    // --------------- O(n) 112s --------------- 33MB --------------- (94% 23%)  ※
+    /*
+     *  easy-understanding , but focus on >=
+     */
+    public bool CheckPossibility_2(int[] nums)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] > nums[i + 1])
+            {
+                if (count == 1)
+                {
+                    return false;
+                }
+
+                count++;
+
+                if (i == 0 || nums[i + 1] >= nums[i - 1])
+                {
+                    nums[i] = nums[i + 1];
+                }
+                else
+                {
+                    nums[i + 1] = nums[i];
+                }
+            }
+        }
+
+        return true;
+    }
 }
 /**************************************************************************************************************
- * CheckPossibility_1                                                                                         *
+ * CheckPossibility_2                                                                                         *
  **************************************************************************************************************/
