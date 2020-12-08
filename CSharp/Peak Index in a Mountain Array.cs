c47bf1fb24f13032b4db7a6e2a6eccad23501f9d
SherryShi0108 <sxr1234567890@outlook.com>
2020-12-08 11:58:19 +0800

    Update Peak Index in a Mountain Array.cs

--- CSharp/Peak Index in a Mountain Array.cs
+++ CSharp/Peak Index in a Mountain Array.cs
@@ -44,7 +44,7 @@ public class Solution852
     /*
      * improve 1
      */
-    public int PeakIndexInMountainArray_2(int[] A)
+    public int PeakIndexInMountainArray_1_2(int[] A)
     {
         int i = 0;
         while (A[i]<A[i+1])
@@ -54,8 +54,28 @@ public class Solution852
 
         return i;
     }
+    
+    // --------------- O(n) 96ms --------------- O(1) 25.7MB --------------- (90% 100%)
+    /*
+     * find the max's index
+     */
+    public int PeakIndexInMountainArray_2(int[] A)
+    {
+        int index = 0;
+        int max = 0;
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (A[i] > max)
+            {
+                max = A[i];
+                index = i;
+            }
+        }
 
-    // --------------- O(logn) 104ms --------------- O(1) 25.9MB --------------- (49% 100%) 
+        return index;
+    }
+
+    // --------------- O(logn) 104ms --------------- O(1) 25.9MB --------------- (49% 100%) ※
     /*
      * use binary search , faster than 1/2 because O(time) = O(logn)
      */
@@ -79,8 +99,8 @@ public class Solution852
         return left;
     }
     
-    // --------------- O(logn) 104ms --------------- O(1) 26MB --------------- (49% 100%) ※
-    public int PeakIndexInMountainArray_4(int[] A)
+    // --------------- O(logn) 104ms --------------- O(1) 26MB --------------- (49% 100%) 
+    public int PeakIndexInMountainArray_3_2(int[] A)
     {
         int i = 0;
         int j = A.Length - 1;
@@ -100,26 +120,8 @@ public class Solution852
         return i;
     }
 
-    // --------------- O(n) 96ms --------------- O(1) 25.7MB --------------- (90% 100%)
-    /*
-     * find the max's index
-     */
-    public int PeakIndexInMountainArray_5(int[] A)
-    {
-        int index = 0;
-        int max = 0;
-        for (int i = 0; i < A.Length; i++)
-        {
-            if (A[i] > max)
-            {
-                max = A[i];
-                index = i;
-            }
-        }
-
-        return index;
-    }
+   
 }
 /**************************************************************************************************************
- * PeakIndexInMountainArray_2 PeakIndexInMountainArray_3 PeakIndexInMountainArray_4                           *
+ * PeakIndexInMountainArray_1 / 2 / 3                                                                         *
  **************************************************************************************************************/
