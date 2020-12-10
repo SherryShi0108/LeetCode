55ab858d028d7cb3ee9682a113cd38e2ec5b7607
SherryShi0108 <sxr1234567890@outlook.com>
2020-12-10 16:17:06 +0800

    Update Squares of a Sorted Array.cs

--- CSharp/Squares of a Sorted Array.cs
+++ CSharp/Squares of a Sorted Array.cs
@@ -36,36 +36,8 @@ public class Solution977
         return A;
     }
 
-    // --------------- O(n) 344ms --------------- 40.4MB --------------- (20% 29%) 
-    public int[] SortedSquares_2(int[] A)
-    {
-        if (A.Length < 1) { return A; }
-
-        int[] B = new int[A.Length];
-        int L = 0;
-        int R = A.Length - 1;
-
-        int i = R;
-        while (L <= R)
-        {
-            if (A[L] * A[L] > A[R] * A[R])
-            {
-                B[i] = A[L] * A[L];
-                L++;
-            }
-            else
-            {
-                B[i] = A[R] * A[R];
-                R--;
-            }
-            i--;
-        }
-
-        return B;
-    }
-
     // --------------- O(n) 304ms --------------- 40.4MB --------------- (84% 29%) ※
-    public int[] SortedSquares_3(int[] A)
+    public int[] SortedSquares_2(int[] A)
     {
         int[] B = new int[A.Length];
         int L = 0;
@@ -86,7 +58,33 @@ public class Solution977
         }
         return B;
     }
+    
+    // --------------- O(n) 288ms --------------- 42MB --------------- (77% 6%) 
+    public int[] SortedSquares_2_2(int[] A)
+    {
+        List<int> list = new List<int>();
+
+        int i = 0;
+        int j = A.Length - 1;
+        while (i <= j)
+        {
+            if (A[i] * A[i] > A[j] * A[j])
+            {
+                list.Add(A[i] * A[i]);
+                i++;
+            }
+            else
+            {
+                list.Add(A[j] * A[j]);
+                j--;
+            }
+        }
+
+        list.Reverse();
+
+        return list.ToArray();
+    }
 }
 /**************************************************************************************************************
- * SortedSquares_3                                                                                            *
+ * SortedSquares_2                                                                                            *
  **************************************************************************************************************/
