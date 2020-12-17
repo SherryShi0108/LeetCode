e4f174ddb05231a470c07f88fff5095768f6d2d0
SherryShi0108 <sxr1234567890@outlook.com>
2020-12-17 10:16:59 +0800

    Update Factorial Trailing Zeroes.cs

--- CSharp/Factorial Trailing Zeroes.cs
+++ CSharp/Factorial Trailing Zeroes.cs
@@ -21,67 +21,11 @@
 
 public class Solution172
 {
-    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++///
-    /*
-     * "2.65252859812191E+32"
-     */
-    public int TrailingZeroes_1(int n)
-    {
-        double mulResult = 1;
-        while (n > 0)
-        {
-            mulResult *= n--;
-        }
-        int count = 0;
-        string s = mulResult.ToString();
-        for (int i = s.Length - 1; i >= 0; i--)
-        {
-            if (s[i] == '0')
-            {
-                count++;
-            }
-            else
-            {
-                break;
-            }
-        }
-        return count;
-    }
-    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++///
-
-    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++///
-    /*
-     * higher than a number , number should be XXXe17 etc.
-     */
-    public int TrailingZeroes_2(int n)
-    {
-        double mulResult = 1;
-        int count = 0;
-        while (n > 0)
-        {
-            mulResult *= n--;
-            while (true)
-            {
-                if (mulResult % 10 == 0)
-                {
-                    count++;
-                    mulResult /= 10;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-        return count;
-    }
-    ///+++++++++++++++++++++++++ Error +++++++++++++++++++++++++///
-
     // --------------- O(n^0.5) 36ms --------------- 12.7MB --------------- (96% 100%) ※
     /*
      * it's amazing solution , all trailing 0 is from factors 5 * 2
      */
-    public int TrailingZeroes_3(int n)
+    public int TrailingZeroes_1(int n)
     {
         int count = 0;
         while (n > 0)
@@ -98,7 +42,7 @@ public class Solution172
      * 3 is better than this solution , because / is greater than * , * can cause overflow 
      * in this solution , should use long i , to avoid overflow number like 1808548329
      */
-    public int TrailingZeroes_4(int n)
+    public int TrailingZeroes_1_2(int n)
     {
         long count = 0;
         for (long i = 5; n / i > 0; i *= 5)
@@ -109,5 +53,5 @@ public class Solution172
     }
 }
 /**************************************************************************************************************
- * TrailingZeroes_3                                                                                           *
+ * TrailingZeroes_1                                                                                           *
  **************************************************************************************************************/
