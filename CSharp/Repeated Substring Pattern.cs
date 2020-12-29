e588cbbb023a0df487abcdaa5df6cd25a9ae3420
SherryShi0108 <sxr1234567890@outlook.com>
2020-12-29 15:30:13 +0800

    Update Repeated Substring Pattern.cs

--- CSharp/Repeated Substring Pattern.cs
+++ CSharp/Repeated Substring Pattern.cs
@@ -22,35 +22,12 @@
  *******************************************************************************************************************************/
 
 public class Solution459
-{
-    /*
-     * time limited , but this solution is true 
-     */
-    public bool RepeatedSubstringPattern_1(string s) 
-    {
-        for (int i = s.Length/2; i >0 ; i--)
-        {
-            if (s.Length % i == 0)
-            {
-                string temp = "";
-                int times = s.Length / i;
-                for (int j = 0; j < times; j++)
-                {
-                    temp += s.Substring(0, i);
-                }
-
-                if (temp.Equals(s)) return true;
-            }
-        }
-
-        return false;
-    }
-    
+{    
     // --------------- O(n^2) 80ms --------------- 31MB --------------- (98% 67%) ※
     /*
      * easy-understanding
      */
-    public bool RepeatedSubstringPattern_2(string s)
+    public bool RepeatedSubstringPattern_1(string s)
     {
         for (int i = 1; i <= s.Length / 2; i++)
         {
@@ -77,12 +54,12 @@ public class Solution459
     /*
      * so tricky , math knowledge
      */
-    public bool RepeatedSubstringPattern_3(string s)
+    public bool RepeatedSubstringPattern_2(string s)
     {
         string str = s + s;
         return str.Substring(1, str.Length - 2).Contains(s);
     }
 }
 /**************************************************************************************************************
- * RepeatedSubstringPattern_2 RepeatedSubstringPattern_3                                                      *
+ * RepeatedSubstringPattern_1 RepeatedSubstringPattern_2                                                      *
  **************************************************************************************************************/
