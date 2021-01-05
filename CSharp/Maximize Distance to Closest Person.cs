5dd6f9d91aa5f193f56fc2ae99cfb2e709ff00e5
SherryShi0108 <sxr1234567890@outlook.com>
2021-01-05 09:48:07 +0800

    Update Maximize Distance to Closest Person.cs

--- CSharp/Maximize Distance to Closest Person.cs
+++ CSharp/Maximize Distance to Closest Person.cs
@@ -31,128 +31,67 @@ using System.Collections.Generic;
 
 public class Solution849
 {
-    // --------------- O(n) 104ms --------------- O(n) 27.2MB --------------- (83% 100%) ※
+    // --------------- O(n) 104ms --------------- O(1) 28MB --------------- (73% 87%) ※
+    /*
+     * before + nomal ; after
+     */
     public int MaxDistToClosest_1(int[] seats)
     {
-        int max = 0;
-        int count = 0;
-        bool flag = true;
+        int last1 = -1;
+        int result = 0;
+
         for (int i = 0; i < seats.Length; i++)
         {
-            if (seats[i] == 0)
+            if (seats[i] == 1)
             {
-                count++;
-            }
-            else
-            {
-                if (flag)
+                if (last1 == -1)
                 {
-                    max = max > count ? max : count;
-                    flag = false;
-                    count = 0;
+                    result = result > i ? result : i;
                 }
                 else
                 {
-                    max = max > (count + 1) / 2 ? max : (count + 1) / 2;
-                    count = 0;
+                    result = result > (i - last1) / 2 ? result : (i - last1) / 2;
                 }
+
+                last1 = i;
             }
         }
 
-        max = max > count ? max : count;
-        return max;
-    }
+        result = result > seats.Length - 1 - last1 ? result : seats.Length - 1 - last1;
+        return result;
+    }   
 
-    // --------------- O(n) 108ms --------------- O(n) 25.9MB --------------- (73% 44%)   
+    // --------------- O(n) 104ms --------------- O(1) 29MB --------------- (73% 87%) 
     /*
-     * easy-understanding
+     * similar to 1
      */
     public int MaxDistToClosest_1_2(int[] seats)
     {
-        int count = 0;
-        int max = 0;
-
-        for (int i = 0; i < seats.Length; i++)
-        {
-            if (seats[i] == 0)
-            {
-                count++;
-                max = max > (count + 1) / 2 ? max : (count + 1) / 2;
-            }
-            else
-            {
-                count = 0;
-            }
-        }
-        for (int i = 0; i < seats.Length; i++)
-        {
-            if (seats[i] == 1)
-            {
-                max = max > i ? max : i;
-                break;
-            }
-        }
-        for (int i = seats.Length - 1; i >= 0; i--)
-        {
-            if (seats[i] == 1)
-            {
-                max = max > seats.Length - 1 - i ? max : seats.Length - 1 - i;
-                break;
-            }
-        }
-        return max;
-    }
-
-    // --------------- O(n) 128ms --------------- O(n) 25.9MB --------------- (11% 47%) 
-    public int MaxDistToClosest_1_3(int[] seats)
-    {
-        int max = 0;
-        int countMedium = 0;
-
-        while (seats[max] == 0)
-        {
-            max++;
-        }
-
-        for (int i = max + 1; i < seats.Length; i++)
-        {
-            if (seats[i] == 0)
-            {
-                countMedium++;
-            }
-            else
-            {
-                max = max > (countMedium + 1) / 2 ? max : (countMedium + 1) / 2;
-                countMedium = 0;
-            }
-        }
-        max = max > countMedium ? max : countMedium;
-        return max;
-    }
-
-    // --------------- O(n) 128ms --------------- O(n) 25.8MB --------------- (11% 58%) 
-    public int MaxDistToClosest_2(int[] seats)
-    {
-        int left = -1;
-        int max = 0;
+        int last1 = -1;
+        int result = 0;
 
         for (int i = 0; i < seats.Length; i++)
         {
             if (seats[i] == 0) continue;
-            if (left == -1)
+
+            if (last1 == -1)
             {
-                max = max > i ? max : i;
+                result = result > i ? result : i;
             }
             else
             {
-                max = max > (i - left) / 2 ? max : (i - left) / 2;
+                result = result > (i - last1) / 2 ? result : (i - last1) / 2;
             }
-            left = i;
+
+            last1 = i;
         }
 
-        max = max > (seats.Length - 1 - left) ? max : (seats.Length - 1 - left);
-        return max;
-    }   
+        if (seats[seats.Length - 1] == 0)
+        {
+            result = result > seats.Length - 1 - last1 ? result : seats.Length - 1 - last1;
+        }
+        return result;
+    }  
 }
 /**************************************************************************************************************
  * MaxDistToClosest_1 / MaxDistToClosest_2                                                                    *
