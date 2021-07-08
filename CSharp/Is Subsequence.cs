d517b8b130ab803a06bc0dbe58fb8f6f5046eae5
SherryShi0108 <sxr1234567890@outlook.com>
2021-07-08 16:26:20 +0800

    Update Is Subsequence.cs

--- CSharp/Is Subsequence.cs
+++ CSharp/Is Subsequence.cs
@@ -28,7 +28,7 @@
 
 public class Solution392
 {
-    // --------------- O(n) 88ms --------------- 37.1MB ---------------(71% 100%) ※
+    // --------------- O(n) 88ms --------------- 37.1MB ---------------(71% 100%) 
     /*
      * great solution!
      */
@@ -47,7 +47,94 @@ public class Solution392
 
         return false;
     }
+    
+    // --------------- O(n) 96ms --------------- 37.1MB ---------------(15% 100%) ※
+    public bool IsSubsequence_1_2(string s, string t)
+    {
+        int i = 0;
+        int j = 0;
+
+        while (i < t.Length && j < s.Length)
+        {
+            if (t[i] == s[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        return j == s.Length;
+    }
+    
+    /* Follow up:  using Dictionary<char,int[]> , int[] invoved each char index in T */
+    // --------------- O(n*m) 128ms --------------- 22MB ---------------(6% 41%) 
+    public bool IsSubsequence_2(string s, string t)
+    {
+        Dictionary<char, List<int>> d = new Dictionary<char, List<int>>();
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (d.ContainsKey(t[i]) == false)
+            {
+                d[t[i]] = new List<int>();
+            }
+            d[t[i]].Add(i);
+        }
+
+        int prev = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (d.ContainsKey(s[i]) == false) return false;
+            int temp = -1;
+            foreach (int i1 in d[s[i]])
+            {
+                if (i1 >= prev)
+                {
+                    temp = i1;
+                    break;
+                }
+            }
+
+            if (temp == -1) return false;
+            prev = temp + 1;
+        }
+
+        return true;
+    }
+    
+    // --------------- O(nlogm) 120ms --------------- 23MB ---------------(7% 16%) 
+    /*
+     * improve: using Array.BinarySearch
+     */
+    public bool IsSubsequence_2_2(string s, string t)
+    {
+        Dictionary<char, List<int>> d = new Dictionary<char, List<int>>();
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (d.ContainsKey(t[i]) == false)
+            {
+                d[t[i]] = new List<int>();
+            }
+
+            d[t[i]].Add(i);
+        }
+
+        int prev = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (d.ContainsKey(s[i]) == false) return false;
+            int temp = Array.BinarySearch(d[s[i]].ToArray(), prev);
+            temp = temp < 0 ? -(temp + 1) : temp; // BinarySearch Method Return Rule
+            if (temp == d[s[i]].Count) return false;
+            prev = d[s[i]][temp] + 1;
+        }
+
+        return true;
+    }
 }
 /**************************************************************************************************************
- * IsSubsequence_1                                                                                            *
+ * IsSubsequence_1     IsSubsequence_2                                                                        *
  **************************************************************************************************************/
